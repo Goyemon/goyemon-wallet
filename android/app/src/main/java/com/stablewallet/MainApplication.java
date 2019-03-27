8c623bd324da1025cc25671a14c177a5d0e2424c
Tai <>
2019-03-27 17:21:16 +0900

    link react-native-keychain package

--- android/app/src/main/java/com/stablewallet/MainApplication.java
+++ android/app/src/main/java/com/stablewallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.stablewallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.keychain.KeychainPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new KeychainPackage(),
             new RNGestureHandlerPackage(),
             new RandomBytesPackage()
       );
