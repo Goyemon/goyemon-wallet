3226cf24557d732682c3e2367a87e79314783f1c
Tai <>
2019-03-31 20:02:36 +0900

    add a receive screen

---
+++ src/components/Receive.js
@@ -0,0 +1,14 @@
+'use strict';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Header } from './common';
+
+export default class Import extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Copy Address</Text>
+      </View>
+    );
+  }
+}
