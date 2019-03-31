8bfd20b2b1fe51078f41cc989eff4686faf92a19
Tai <>
2019-03-31 20:02:21 +0900

    add a send and receive button in ethereum wallet

--- src/components/Ethereum.js
+++ src/components/Ethereum.js
@@ -6,8 +6,22 @@ import { Header } from './common';
 export default class Import extends Component {
   render() {
     return (
+      <View>
       <View style={styles.textStyle}>
-        <Text>Ethereum</Text>
+        <Text>0 ETH</Text>
+      </View>
+      <View>
+        <Button
+          title="Send"
+          onPress={() => this.props.navigation.navigate('Send')}
+        />
+      </View>
+      <View>
+        <Button
+          title="Receive"
+          onPress={() => this.props.navigation.navigate('Receive')}
+        />
+        </View>
       </View>
     );
   }
