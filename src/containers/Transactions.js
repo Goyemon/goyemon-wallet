5e3b79c096ad916752409280af12edb3f01e36df
Tai <>
2019-04-24 16:09:00 +0900

    add a Transactions component

---
+++ src/containers/Transactions.js
@@ -0,0 +1,60 @@
+'use strict';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Card, CardSection } from '../components/common';
+import { connect } from "react-redux";
+import { getTransactions } from "../actions/ActionTransactions";
+
+class Transactions extends Component {
+  componentDidMount() {
+    this.props.getTransactions();
+  }
+
+  render() {
+    const { id, title, completed } = this.props.transaction;
+    const {
+      TransactionListStyle,
+      WalletStyleMiddleContainer,
+      textStyle
+    } = styles;
+    return (
+      <Card>
+        <CardSection>
+          <View style={TransactionListStyle}>
+            <Text style={[WalletStyleMiddleContainer, textStyle]}>{id}</Text>
+            <Text style={[WalletStyleMiddleContainer, textStyle]}>{title}</Text>
+            <Text style={[WalletStyleMiddleContainer, textStyle]}>{title}</Text>
+            <Text style={[WalletStyleMiddleContainer, textStyle]}>{title}</Text>
+            <Text style={[WalletStyleMiddleContainer, textStyle]}>{title}</Text>
+          </View>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  TransactionListStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  WalletStyleMiddleContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  textStyle: {
+    fontSize: 16
+  }
+};
+
+const mapStateToProps = state => {
+  return { transactions: state.ReducerTransactions.transactions }
+}
+
+const mapDispatchToProps = {
+    getTransactions
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Transactions);
