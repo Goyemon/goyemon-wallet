d4720ee462a711895e55c087afdf5b894ecf688c
Tai <>
2019-04-24 16:10:14 +0900

    add the GET_TRANSACTIONS action type

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,3 +1,4 @@
 export const GET_MNEMONIC = "GET_MNEMONIC";
 export const GET_ETH_PRICE = "GET_ETH_PRICE";
 export const GET_DAI_PRICE = "GET_DAI_PRICE";
+export const GET_TRANSACTIONS = "GET_TRANSACTIONS";
