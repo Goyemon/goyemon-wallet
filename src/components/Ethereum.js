6e69e71c14f34c8643694693aa1c9f997b5304e2
Tai <>
2019-04-24 16:15:30 +0900

    add a Transactions component to Ethereum component

--- src/components/Ethereum.js
+++ src/components/Ethereum.js
@@ -1,36 +1,64 @@
 'use strict';
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-import { Header } from './common';
+import React from 'react';
+import { connect } from 'react-redux';
+import { withNavigation } from 'react-navigation';
+import { ScrollView, View, Text, Button } from 'react-native';
+import Transactions from '../containers/Transactions';
 
-export default class Import extends Component {
-  render() {
+const Ethereum = ({ transactions, navigation }) => {
+  const { textStyle, TransactionListStyle, WalletStyleMiddleContainer } = styles;
     return (
-      <View>
-      <View style={styles.textStyle}>
-        <Text>0 ETH</Text>
-      </View>
-      <View>
-        <Button
-          title="Send"
-          onPress={() => this.props.navigation.navigate('Send')}
-        />
-      </View>
-      <View>
-        <Button
-          title="Receive"
-          onPress={() => this.props.navigation.navigate('Receive')}
-        />
+      <ScrollView>
+        <View>
+        <View style={textStyle}>
+          <Text>0 ETH</Text>
         </View>
-      </View>
+        <View>
+          <Button
+            title="Send"
+            onPress={() => navigation.navigate('Send')}
+          />
+        </View>
+        <View>
+          <Button
+            title="Receive"
+            onPress={() => navigation.navigate('Receive')}
+          />
+        </View>
+        </View>
+        <View style={TransactionListStyle}>
+          <Text style={[WalletStyleMiddleContainer, textStyle]}>transaction id</Text>
+          <Text style={[WalletStyleMiddleContainer, textStyle]}>status</Text>
+          <Text style={[WalletStyleMiddleContainer, textStyle]}>amount</Text>
+          <Text style={[WalletStyleMiddleContainer, textStyle]}>to</Text>
+          <Text style={[WalletStyleMiddleContainer, textStyle]}>date</Text>
+        </View>
+        {transactions.map(transaction => (<Transactions key={transaction.id} transaction={transaction} />
+        ))}
+      </ScrollView>
     );
-  }
-}
+};
 
 const styles = {
   textStyle: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  TransactionListStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  WalletStyleMiddleContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
   }
 };
+
+const mapStateToProps = state => {
+  return { transactions: state.ReducerTransactions.transactions }
+}
+
+export default withNavigation(connect(mapStateToProps)(Ethereum));
