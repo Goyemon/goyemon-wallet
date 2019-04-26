e86a4b4ef93b199ac35db8cc582838cfb3b9fb81
Tai <>
2019-04-26 18:45:31 +0900

    redesign a common button component with styled-components

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,35 +1,24 @@
 'use strict';
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import styled from 'styled-components';
 
-const Button = ({ onPress, children }) => {
-  const { buttonStyle, textStyle } = styles;
-  return (
-    <TouchableOpacity onPress={onPress} style={buttonStyle}>
-      <Text style={textStyle}>{children}</Text>
-    </TouchableOpacity>
-  );
-};
+const Button = props => (
+  <ButtonContainer onPress={props.onPress} backgroundColor={props.backgroundColor}>
+    <ButtonText textColor={props.textColor}>{props.text}</ButtonText>
+  </ButtonContainer>
+);
 
-const styles = {
-  buttonStyle: {
-    flex: 1,
-    alignSelf: 'stretch',
-    backgroundColor: '#FFF',
-    borderRadius: 6,
-    borderWidth: 1,
-    borderColor: '#007AFF',
-    marginLeft: 8,
-    marginRight: 8
-  },
-  textStyle: {
-    alignSelf: 'center',
-    color: '#007AFF',
-    fontSize: 16,
-    fontWeight: '600',
-    paddingTop: 12,
-    paddingBottom: 12
-  }
-};
+const ButtonContainer = styled.TouchableOpacity`
+    width: 160px;
+    border-radius: 8px;
+    background-color: ${props => props.backgroundColor};
+`;
+
+const ButtonText = styled.Text`
+  font-size: 16px;
+  color: ${props => props.textColor};
+  padding: 16px;
+  text-align: center;
+`;
 
 export { Button };
