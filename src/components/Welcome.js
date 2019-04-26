2661e5f6d24de0b0fe5ea132842c99138966e611
Tai <>
2019-04-26 18:48:32 +0900

    animated texts in Welcome screen

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,26 +1,28 @@
 'use strict';
 import React, { Component } from 'react';
-import { Button } from 'react-native';
-import styled from 'styled-components';
+import { Button } from '../components/common/Button';
+import styled from 'styled-components/native';
+import * as Animatable from 'react-native-animatable';
 
 export default class Welcome extends Component {
   render() {
     return (
       <Container>
-        <Title>Holla!</Title>
-        <Title>ARE YOU READY?</Title>
-        <Button title="Hell Yeah!" onPress={() => this.props.navigation.navigate('Start')} />
+        <Title animation="fadeInDown" delay={1000}>Holla!</Title>
+        <Title animation="fadeIn" delay={2000}>ARE YOU READY?</Title>
+        <Button text="Hell Yeah!" textColor="white" backgroundColor="#01d1e5" onPress={() => this.props.navigation.navigate('Start')} />
       </Container>
     );
   }
 }
 
-const Container = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-`;
+const Container = Animatable.createAnimatableComponent(styled.View`
+  flex: 1;
+  font-size: 40px;
+  justify-content: center;
+  align-items: center;
+`);
 
-const Title = styled.Text`
-    font-size: 40px;
-`;
+const Title = Animatable.createAnimatableComponent(styled.Text`
+  font-size: 40px;
+`);
