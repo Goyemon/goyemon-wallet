708c3311837ce7185a3357f107b375108dcf66fb
Tai <>
2019-04-27 14:47:56 +0900

    create ChecksumAddress reducer

---
+++ src/reducers/ReducerChecksumAddress.js
@@ -0,0 +1,17 @@
+'use strict';
+import { GET_CHECKSUM_ADDRESS } from "../constants/ActionTypes";
+
+const INITIAL_STATE = {
+  checksumAddress: ""
+};
+
+const checksumAddress = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case GET_CHECKSUM_ADDRESS:
+      return { ...state, checksumAddress: action.payload };
+    default:
+      return state || INITIAL_STATE;
+  };
+};
+
+export default checksumAddress;
--- src/reducers/ReducerIndex.js
+++ src/reducers/ReducerIndex.js
@@ -4,11 +4,13 @@ import { combineReducers } from 'redux';
 import ReducerMnemonic from './ReducerMnemonic';
 import ReducerWallets from './ReducerWallets';
 import ReducerTransactions from './ReducerTransactions';
+import ReducerChecksumAddress from './ReducerChecksumAddress';
 
 const rootReducers = combineReducers({
   ReducerMnemonic,
   ReducerWallets,
   ReducerTransactions
+  ReducerChecksumAddress
 });
 
 export default rootReducers;
