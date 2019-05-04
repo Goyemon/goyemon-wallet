aa626b71e13255894ed6f346564d1a13b3c86577
Tai <>
2019-05-04 13:38:01 +0900

    add necessary component from react native

--- src/components/Send.js
+++ src/components/Send.js
@@ -1,6 +1,6 @@
 'use strict';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Header } from './common';
 import { Button } from '../components/common/Button';
 import { connect } from "react-redux";
