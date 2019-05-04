9e67ffde44018abba8ff8581a6b49b42688e2f6f
Tai <>
2019-05-04 14:01:17 +0900

    copy the address to clipboard animation in Receive screen

--- src/components/Receive.js
+++ src/components/Receive.js
@@ -1,17 +1,38 @@
 'use strict';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Clipboard, TouchableWithoutFeedback } from 'react-native';
 import { Header } from './common';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import QRCode from 'react-native-qrcode';
+import * as Animatable from 'react-native-animatable';
 
 class Receive extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clipboardContent: null
+    };
+    this.AnimationRef;
+  }
+
+  onPress() {
+    this.AnimationRef.jello();
+  }
+
+  async writeToClipboard() {
+    await Clipboard.setString(this.props.checksumAddress);
+  };
+
   render() {
     const checksumAddress = this.props.checksumAddress;
     return (
       <View>
-      <Text>Address: {checksumAddress}</Text>
-      <Text>Copy Address</Text>
+        <Text>Address: {checksumAddress}</Text>
+        <TouchableWithoutFeedback onPress={async () => { await this.onPress(); this.writeToClipboard()}}>
+          <Animatable.View ref={ref => (this.AnimationRef = ref)}>
+            <Text>Copy Address</Text>
+          </Animatable.View>
+        </TouchableWithoutFeedback>
         <QRCode value={checksumAddress} size={200} bgColor="#000" fgColor="#FFF" />
       </View>
     );
@@ -20,7 +41,7 @@ class Receive extends Component {
 
 function mapStateToProps(state) {
   return {
-    checksumAddress: state.ReducerChecksumAddress.checksumAddress,
+    checksumAddress: state.ReducerChecksumAddress.checksumAddress
   };
 }
 
