3c80b914c5649941bab514970d3baf0b3ae183fc
Tai <>
2019-05-06 18:22:25 +0900

    build the structure of Setting screen with native-base

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,27 +1,56 @@
 'use strict';
 import React, { Component } from 'react';
-import { Header } from './common';
-import styled from 'styled-components/native';
+import { Container, Button, Header, Content, List, ListItem, Text, Icon, Left, Body, Right, Switch } from 'native-base';
 
 export default class Settings extends Component {
   render() {
     return (
       <Container>
-        <Title>Notifications</Title>
-        <Title>Help</Title>
-        <Title>About</Title>
+        <Header />
+        <Content>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#FF9501" }}>
+                <Icon active name="airplane" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>Notifications</Text>
+            </Body>
+            <Right>
+              <Switch value={false} />
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#007AFF" }}>
+                <Icon active name="wifi" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>Help</Text>
+            </Body>
+            <Right>
+              <Text>haha</Text>
+              <Icon active name="arrow-forward" />
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#007AFF" }}>
+                <Icon active name="bluetooth" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>About</Text>
+            </Body>
+            <Right>
+              <Text>On</Text>
+              <Icon active name="arrow-forward" />
+            </Right>
+          </ListItem>
+        </Content>
       </Container>
     );
   }
 }
-
-const Container = styled.View`
-  flex: 1;
-  font-size: 40px;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  font-size: 40px;
-`;
