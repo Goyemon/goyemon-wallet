643295be69bae7c182d0e65c95f42d9d69d37b48
Tai <>
2019-05-18 11:37:06 +0900

    remove native-base package and add react-native-settings-list

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,56 +1,17 @@
 'use strict';
 import React, { Component } from 'react';
-import { Container, Button, Header, Content, List, ListItem, Text, Icon, Left, Body, Right, Switch } from 'native-base';
+import { View, Text, Image } from 'react-native';
+import SettingsList from 'react-native-settings-list';
 
 export default class Settings extends Component {
   render() {
     return (
-      <Container>
-        <Header />
-        <Content>
-          <ListItem icon>
-            <Left>
-              <Button style={{ backgroundColor: "#FF9501" }}>
-                <Icon active name="airplane" />
-              </Button>
-            </Left>
-            <Body>
-              <Text>Notifications</Text>
-            </Body>
-            <Right>
-              <Switch value={false} />
-            </Right>
-          </ListItem>
-          <ListItem icon>
-            <Left>
-              <Button style={{ backgroundColor: "#007AFF" }}>
-                <Icon active name="wifi" />
-              </Button>
-            </Left>
-            <Body>
-              <Text>Help</Text>
-            </Body>
-            <Right>
-              <Text>haha</Text>
-              <Icon active name="arrow-forward" />
-            </Right>
-          </ListItem>
-          <ListItem icon>
-            <Left>
-              <Button style={{ backgroundColor: "#007AFF" }}>
-                <Icon active name="bluetooth" />
-              </Button>
-            </Left>
-            <Body>
-              <Text>About</Text>
-            </Body>
-            <Right>
-              <Text>On</Text>
-              <Icon active name="arrow-forward" />
-            </Right>
-          </ListItem>
-        </Content>
-      </Container>
+      <View>
+        <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
+          <SettingsList.Header headerStyle={{marginTop:16}}/>
+          <SettingsList.Header headerStyle={{marginTop:16}}/>
+        </SettingsList>
+      </View>
     );
   }
 }
