be9ba927176676d1f03bf8ad82f47385df110af1
Tai <>
2019-05-18 12:14:25 +0900

    add ReducerTransactionObject

--- src/reducers/ReducerIndex.js
+++ src/reducers/ReducerIndex.js
@@ -7,14 +7,16 @@ import ReducerWallets from './ReducerWallets';
 import ReducerTransactions from './ReducerTransactions';
 import ReducerChecksumAddress from './ReducerChecksumAddress';
 import ReducerGasPrice from './ReducerGasPrice';
+import ReducerTransactionObject from './ReducerTransactionObject';
 
 const rootReducers = combineReducers({
   ReducerWeb3,
   ReducerMnemonic,
   ReducerWallets,
-  ReducerTransactions
-  ReducerChecksumAddress
-  ReducerGasPrice
+  ReducerTransactions,
+  ReducerChecksumAddress,
+  ReducerGasPrice,
+  ReducerTransactionObject
 });
 
 export default rootReducers;
---
+++ src/reducers/ReducerTransactionObject.js
@@ -0,0 +1,17 @@
+'use strict';
+import { SAVE_TRANSACTION_OBJECT } from "../constants/ActionTypes";
+
+const INITIAL_STATE = {
+  transactionObject: {}
+};
+
+const transactionObject = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case SAVE_TRANSACTION_OBJECT:
+      return { ...state, transactionObject: action.payload };
+    default:
+      return state || INITIAL_STATE;
+  };
+};
+
+export default transactionObject;
