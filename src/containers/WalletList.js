f245790d05d16cdececad9d1b2487b2c2a014934
Tai <>
2019-05-18 12:00:54 +0900

    get balance in ether and usd in WalletList screen

--- src/containers/WalletList.js
+++ src/containers/WalletList.js
@@ -1,39 +1,109 @@
 'use strict';
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withNavigation } from 'react-navigation';
 import { ScrollView, TouchableOpacity, View, Text } from 'react-native';
 import WalletDetail from '../containers/WalletDetail';
+import styled from 'styled-components';
+import { getEthPrice, getDaiPrice } from "../actions/ActionWallets";
 
-const WalletList = ({ wallets, navigation }) => {
-  const { textStyle } = styles;
-  return (
-    <ScrollView style={textStyle}>
-      <View><Text>Balance</Text></View>
-      {wallets.map(wallet => (
-        <TouchableOpacity
-          key={wallet.id}
-          onPress={
-            wallet.coin === 'Ether'
-              ? () => navigation.navigate('Ethereum')
-              : () => navigation.navigate('Dai')
-          }
-        >
-          <WalletDetail key={wallet.id} wallet={wallet} />
-        </TouchableOpacity>
-      ))}
-    </ScrollView>
-  );
-};
-
-const styles = {
-  textStyle: {
-    backgroundColor: '#FFF'
+class WalletList extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      balance: "0.0",
+      usdBalance: "0.0"
+    };
   }
-};
 
-const mapStateToProps = state => {
-  return { wallets: state.ReducerWallets.wallets }
+  async componentDidMount() {
+    await this.props.getEthPrice();
+    const newBalanceInWei = await this.getBalance(this.props.checksumAddress);
+    const newBalanceInEther = await this.props.web3.utils.fromWei(newBalanceInWei, 'ether');
+    await this.setState({balance: newBalanceInEther});
+    await this.setState({usdBalance: await this.getUsdBalance()});
+  }
+
+  async getBalance(address) {
+    try {
+      const balance = await this.props.web3.eth.getBalance(address);
+      return balance;
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  async getUsdBalance() {
+    try {
+      const usdPrice = this.props.wallets[0].price;
+      const ethBalance = parseFloat(this.state.balance);
+      const usdBalance = usdPrice * ethBalance;
+      return usdBalance;
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    const { wallets, navigation } = this.props;
+
+    if(!this.props.web3.eth){
+      return <Text>loading...</Text>;
+    };
+
+    return (
+      <ScrollView>
+        <CardContainerWithoutFeedback>
+          <Text>TOTAL BALANCE</Text>
+          <Text>{this.state.balance} ETH</Text>
+          <Text>{this.state.usdBalance} USD</Text>
+        </CardContainerWithoutFeedback>
+        {wallets.map(wallet => (
+          <CardContainer
+            key={wallet.id}
+            onPress={
+              wallet.coin === 'Ether'
+                ? () => navigation.navigate('Ethereum')
+                : () => navigation.navigate('Dai')
+            }
+          >
+            <WalletDetail key={wallet.id} wallet={wallet} />
+          </CardContainer>
+        ))}
+      </ScrollView>
+    );
+  }
 }
 
-export default withNavigation(connect(mapStateToProps)(WalletList));
+const CardContainer = styled.TouchableOpacity`
+  width: 160px;
+  border-radius: 8px;
+  background: #FFF;
+  width: 320px;
+  height: 160px;
+  margin: 24px 16px;
+  align-items: center;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+`;
+
+const CardContainerWithoutFeedback = styled.View`
+  background: #FFF;
+  height: 160px;
+  align-items: center;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+`;
+
+const mapStateToProps = state => {
+  return {
+    wallets: state.ReducerWallets.wallets,
+    web3: state.ReducerWeb3.web3,
+    checksumAddress: state.ReducerChecksumAddress.checksumAddress
+  }
+}
+
+const mapDispatchToProps = {
+    getEthPrice,
+    getDaiPrice
+}
+
+export default withNavigation(connect(mapStateToProps, mapDispatchToProps)(WalletList));
