8424936ee17fe29ab3365cc4e33d2e89aafd6a26
Tai <>
2019-05-18 11:40:27 +0900

    style ShowMnemonic screen

--- src/containers/ShowMnemonic.js
+++ src/containers/ShowMnemonic.js
@@ -1,49 +1,60 @@
 'use strict';
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-import { connect } from "react-redux";
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import styled from 'styled-components/native';
+import { Button } from '../components/common';
 
 class ShowMnemonic extends Component {
   render() {
-    const mnemonic = this.props.mnemonic;
+    const { mnemonic } = this.props;
+    const splitMnemonic = mnemonic.split(' ');
+
     return (
-      <View>
-        <View style={styles.containerStyle}>
-          <Text>This is your mnemonic code.</Text>
-          <Text>Keep it somewhere safe and never lose it!</Text>
-          <Text style={styles.showMnemonicStyle}>{mnemonic}</Text>
-          <Button
-            title="I memorized menomic codes!"
-            onPress={() => this.props.navigation.navigate('VerifyMnemonic')}
-          />
-        </View>
+      <View style={styles.textStyle}>
+        <Text>backup phrases</Text>
+        <Text>Please carefully write down these 24 words.</Text>
+        <MnemonicPhrasesContainer>
+          {splitMnemonic.map(splitMnemonic => (
+            <MnemonicWord>{splitMnemonic} </MnemonicWord>
+          ))}
+        </MnemonicPhrasesContainer>
+        <Text>we will confirm this phrase on the next screen</Text>
+        <Button
+          text="I’ve written them down"
+          textColor="white" backgroundColor="#01d1e5"
+          marginTop="24px"
+          onPress={() => this.props.navigation.navigate('VerifyMnemonic')}to 
+        />
       </View>
     );
   }
 }
 
-
 const styles = {
-  containerStyle: {
+  textStyle: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  showMnemonicStyle: {
-    fontSize: 24,
-    fontWeight: '600',
-    lineHeight: 32,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 8,
-    padding: 8,
-    margin: 8
   }
 };
 
+const MnemonicPhrasesContainer = styled.Text`
+  flexDirection: row;
+  justifyContent: center;
+  alignItems: center;
+`;
+
+const MnemonicWord = styled.Text`
+  border-radius: 8px;
+  background: #4a90e2;
+  margin: 4px;
+`;
+
 function mapStateToProps(state) {
   return {
-    mnemonic: state.ReducerMnemonic.mnemonic,
+    mnemonic: state.ReducerMnemonic.mnemonic
   };
 }
 
- export default connect(mapStateToProps)(ShowMnemonic);
+export default connect(mapStateToProps)(ShowMnemonic);
