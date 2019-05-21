b3e883d1d8a8493a3c2f247612b6bd8bb84b59da
Tai <>
2019-05-21 16:59:31 +0900

    rebuild android

--- android/app/src/main/java/com/debank/MainApplication.java
+++ android/app/src/main/java/com/debank/MainApplication.java
@@ -3,6 +3,7 @@ package com.debank;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.peel.react.rnos.RNOSModule;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.keychain.KeychainPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNOSModule(),
             new RNGestureHandlerPackage(),
             new RandomBytesPackage(),
             new KeychainPackage()
