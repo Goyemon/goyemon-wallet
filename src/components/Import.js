525c205226c68072cb562932c1976817cbc84b51
Tai <>
2019-05-28 17:51:19 +0900

    build a form in Import container

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,5 +1,6 @@
 'use strict';
 import React, { Component } from 'react';
+import { View, TextInput, Text } from 'react-native';
 import { Header } from './common';
 import { connect } from 'react-redux';
 import styled from 'styled-components/native';
@@ -8,11 +9,37 @@ import { saveWeb3 } from '../actions/ActionWeb3';
 import { getChecksumAddress } from '../actions/ActionChecksumAddress';
 
 class Import extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mnemonicPhrases: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" ]
+    };
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange = (text, id) => {
+    let mnemonicPhrases = this.state.mnemonicPhrases;
+    mnemonicPhrases[id] = text;
+
+    this.setState({mnemonicPhrases: mnemonicPhrases});
+  }
+
   render() {
     return (
       <Container>
-        <Title>Choose your wallet</Title>
-        <View>
+        <Title>Enter the backup phrases for the wallet you want to import</Title>
+        {this.state.mnemonicPhrases.map((word, id) => (
+          <TextInputContainer key={id}>
+              <TextInput
+                placeholder = "word"
+                autoCapitalize = 'none'
+                maxLength = {15}
+                onChangeText = {(text) => {this.handleTextChange(text, id)} }
+              />
+              <Text>{id + 1}</Text>
+          </TextInputContainer>
+        ))}
+        <ButtonContainer>
           <Button
             text="go"
             textColor="white"
@@ -22,21 +49,30 @@ class Import extends Component {
               await this.validateForm()
             }}
           />
-        </View>
+        </ButtonContainer>
       </Container>
     );
   }
 }
 
-const Container = styled.View`
-  flex: 1;
+const Container = styled.ScrollView`
   font-size: 40px;
-  justify-content: center;
-  align-items: center;
 `;
 
 const Title = styled.Text`
-  font-size: 40px;
+  font-size: 32px;
+`;
+
+const TextInputContainer = styled.View`
+  flexDirection: column;
+  justifyContent: center;
+  alignItems: center;
+`;
+
+const ButtonContainer = styled.View`
+  flexDirection: row;
+  justifyContent: center;
+  alignItems: center;
 `;
 
 const mapDispatchToProps = {
