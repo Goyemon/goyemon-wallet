67f6e2c076af8657a2080ff26808ed450c0bd812
Tai <>
2019-06-10 15:44:56 +0900

    rebuild ios

---
+++ ios/build/DeBank/Build/Products/Debug-iphonesimulator/DeBank.app/react-native-keychain/typings/react-native-keychain.d.ts
@@ -0,0 +1,97 @@
+declare module 'react-native-keychain' {
+
+    export interface UserCredentials {
+        username: string;
+        password: string;
+    }
+
+    export interface SharedWebCredentials {
+        server: string;
+        username: string;
+        password: string;
+    }
+
+    export enum ACCESSIBLE   {
+        WHEN_UNLOCKED = "AccessibleWhenUnlocked",
+        AFTER_FIRST_UNLOCK = "AccessibleAfterFirstUnlock",
+        ALWAYS = "AccessibleAlways",
+        WHEN_PASSCODE_SET_THIS_DEVICE_ONLY = "AccessibleWhenPasscodeSetThisDeviceOnly",
+        WHEN_UNLOCKED_THIS_DEVICE_ONLY = "AccessibleWhenUnlockedThisDeviceOnly",
+        AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY = "AccessibleAfterFirstUnlockThisDeviceOnly",
+        ALWAYS_THIS_DEVICE_ONLY = "AccessibleAlwaysThisDeviceOnly"
+    }
+
+    export enum ACCESS_CONTROL {
+        USER_PRESENCE = "UserPresence",
+        BIOMETRY_ANY = "BiometryAny",
+        BIOMETRY_CURRENT_SET = "BiometryCurrentSet",
+        DEVICE_PASSCODE = "DevicePasscode",
+        APPLICATION_PASSWORD = "ApplicationPassword",
+        BIOMETRY_ANY_OR_DEVICE_PASSCODE = "BiometryAnyOrDevicePasscode",
+        BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE = "BiometryCurrentSetOrDevicePasscode"
+    }
+
+    export enum LAPolicy {
+        DEVICE_PASSCODE_OR_BIOMETRICS = "AuthenticationWithBiometricsDevicePasscode",
+        BIOMETRICS = "AuthenticationWithBiometrics"
+    }
+
+    export interface Options {
+        accessControl?: ACCESS_CONTROL;
+        accessGroup?: string;
+        accessible?: ACCESSIBLE;
+        authenticationPrompt?: string;
+        authenticationType?: LAPolicy;
+        service?: string;
+    }
+
+    function canImplyAuthentication(
+        options?: Options
+    ): Promise<boolean>;
+
+    function getSupportedBiometryType(
+    ): Promise<string>;
+
+    function setInternetCredentials(
+        server: string,
+        username: string,
+        password: string,
+        options?: Options
+    ): Promise<void>;
+
+    function getInternetCredentials(
+        server: string
+    ): Promise<UserCredentials>;
+
+    function hasInternetCredentials(
+        server: string
+    ): Promise<boolean>;
+
+    function resetInternetCredentials(
+        server: string
+    ): Promise<void>;
+
+    function setGenericPassword(
+        username: string,
+        password: string,
+        options?: Options
+    ): Promise<boolean>;
+
+    function getGenericPassword(
+        options?: Options
+    ): Promise<false | {service: string, username: string, password: string}>;
+
+    function resetGenericPassword(
+        options?: Options
+    ): Promise<boolean>
+
+    function requestSharedWebCredentials (
+    ): Promise<SharedWebCredentials>;
+
+    function setSharedWebCredentials(
+        server: string,
+        username: string,
+        password: string
+    ): Promise<void>;
+
+}
