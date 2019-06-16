6d570643cf61496a506717fd5b905e19a3822af1
Tai <>
2019-06-16 13:23:08 +0900

    style the Welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,36 +1,39 @@
 'use strict';
 import React, { Component } from 'react';
-import { Button } from '../components/common';
+import { RootContainer, Button } from '../components/common';
 import styled from 'styled-components/native';
 import * as Animatable from 'react-native-animatable';
 
 export default class Welcome extends Component {
   render() {
     return (
-      <Container>
-        <Title animation="fadeInDown" delay={500}>Holla!</Title>
-        <Title animation="fadeIn" delay={2000}>READY TO JOIN  THE UNIVERSE?</Title>
-        <ButtonWrapper animation="fadeIn" delay={4000}>
-          <Button
-          text="Hell Yeah!"
-          textColor="white" backgroundColor="#01d1e5"
-          onPress={() => this.props.navigation.navigate('Start')} />
-        </ButtonWrapper>
-      </Container>
+      <RootContainer>
+        <Container>
+          <Title animation="fadeInDown" delay={500}>Holla!</Title>
+          <Title animation="fadeIn" delay={2000}>Ready to join ?</Title>
+          <ButtonWrapper animation="fadeIn" delay={4000}>
+            <Button
+            text="Hell Yeah!"
+            textColor="white" backgroundColor="#4083FF"
+            margin="16px auto"
+            onPress={() => this.props.navigation.navigate('Start')} />
+          </ButtonWrapper>
+        </Container>
+      </RootContainer>
     );
   }
 }
 
 const Container = styled.View`
   flex: 1;
-  font-size: 40px;
   justify-content: center;
+  margin-top: 240px;
   text-align: center;
 `;
 
 const Title = Animatable.createAnimatableComponent(styled.Text`
   font-size: 40px;
-  margin-bottom: 40px;
+  margin-bottom: 16px;
   text-align: center;
 `);
 
