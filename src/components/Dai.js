63886cf76febd44b9df28472a764b1f8a69cec21
Tai <>
2019-06-16 13:16:44 +0900

    style the Dai component

--- src/components/Dai.js
+++ src/components/Dai.js
@@ -1,25 +1,16 @@
 'use strict';
 import React, { Component } from 'react';
-import { Header } from './common';
+import { RootContainer, HeaderOne } from './common';
 import styled from 'styled-components/native';
 
 export default class Dai extends Component {
   render() {
     return (
-      <Container>
-        <Title>Dai</Title>
-      </Container>
+      <RootContainer>
+        <HeaderOne>
+          Dai
+        </HeaderOne>
+      </RootContainer>
     );
   }
 }
-
-const Container = styled.View`
-  flex: 1;
-  font-size: 40px;
-  justify-content: center;
-  text-align: center;
-`;
-
-const Title = styled.Text`
-  font-size: 40px;
-`;
