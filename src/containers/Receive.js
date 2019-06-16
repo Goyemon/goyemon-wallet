49eb493d4b48adee554822e46569c72fae3e7f5a
Tai <>
2019-06-16 13:34:06 +0900

    style the Receive component

--- src/containers/Receive.js
+++ src/containers/Receive.js
@@ -1,7 +1,7 @@
 'use strict';
 import React, { Component } from 'react';
 import { View, Text, Clipboard, TouchableWithoutFeedback, Image } from 'react-native';
-import { Header } from '../components/common';
+import { RootContainer, UntouchableCardContainer, HeaderOne } from '../components/common';
 import { connect } from 'react-redux';
 import QRCode from 'react-native-qrcode';
 import * as Animatable from 'react-native-animatable';
@@ -27,49 +27,28 @@ class Receive extends Component {
   render() {
     const { checksumAddress } = this.props;
     return (
-      <View>
-        <CardContainerWithoutFeedback>
-          <AddressContainer>
-            <Text>Address: {checksumAddress}</Text>
-            <TouchableWithoutFeedback onPress={async () => { await this.onPress(); this.writeToClipboard()}}>
-              <Animatable.View ref={ref => (this.AnimationRef = ref)}>
-                <Image source={require('../../assets/copy_icon.png')} />
-              </Animatable.View>
-            </TouchableWithoutFeedback>
-          </AddressContainer>
-        </CardContainerWithoutFeedback>
-        <CardContainerWithoutFeedback>
-          <QRCodeContainer>
-            <QRCode value={checksumAddress} size={200} bgColor="#000" fgColor="#FFF" />
-          </QRCodeContainer>
-        </CardContainerWithoutFeedback>
-      </View>
+      <RootContainer>
+        <HeaderOne>Receive</HeaderOne>
+        <UntouchableCardContainer
+          alignItems="center"
+          flexDirection="column"
+          height="240px"
+          justifyContent="flex-start"
+          textAlign="left"
+          width="95%"
+         >
+          <Text>Address: {checksumAddress}</Text>
+          <TouchableWithoutFeedback onPress={async () => { await this.onPress(); this.writeToClipboard()}}>
+            <Animatable.View ref={ref => (this.AnimationRef = ref)}>
+              <Image source={require('../../assets/copy_icon.png')} />
+            </Animatable.View>
+          </TouchableWithoutFeedback>
+          <QRCode value={checksumAddress} size={120} bgColor="#000" fgColor="#FFF" />
+        </UntouchableCardContainer>
+      </RootContainer>
     );
   }
 }
-
-const CardContainerWithoutFeedback = styled.View`
-  width: 160px;
-  border-radius: 8px;
-  background: #FFF;
-  width: 360px;
-  height: 200px;
-  margin: 24px 16px;
-  text-align: left;
-  box-shadow: 0 2px 5px rgba(0,0,0,0.3);
-`;
-
-const AddressContainer = styled.View`
-  flexDirection: row;
-  justifyContent: center;
-  alignItems: center;
-`;
-
-const QRCodeContainer = styled.View`
-  flexDirection: row;
-  justifyContent: center;
-  alignItems: center;
-`;
 
 function mapStateToProps(state) {
   return {
