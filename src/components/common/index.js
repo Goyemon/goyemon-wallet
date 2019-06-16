a9cabd523ab6d348f8e67435d1b6e9dfd0677a39
Tai <>
2019-06-16 13:15:49 +0900

    delete the Header common component from index.js

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,7 +1,6 @@
 export * from './RootContainer';
 export * from './Button';
 export * from './Card';
-export * from './Header';
 export * from './Input';
 export * from './Spinner';
 export * from './UntouchableCardContainer';
