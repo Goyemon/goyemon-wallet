614de5520a5d42e26e78e2f0021c6a0f7d80bf39
Tai <>
2019-07-10 21:27:17 +0900

    add redux-persist and persist transaction history and outgoing transaction object states

--- src/navigators/AppTab.js
+++ src/navigators/AppTab.js
@@ -5,11 +5,25 @@ import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import HomeStack from './HomeStack';
 import SettingsStack from './SettingsStack';
 import rootReducers from '../reducers/ReducerIndex';
 
-const store = createStore(rootReducers, applyMiddleware(thunk));
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['ReducerOutgoingTransactionObject', 'ReducerTransactionHistory']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+let store = createStore(persistedReducer, applyMiddleware(thunk));
+
+let persistor = persistStore(store);
+
 const AppTab = createBottomTabNavigator(
   {
     Home: {
@@ -45,6 +59,8 @@ const App = createAppContainer(AppTab);
 
 export default () => (
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
