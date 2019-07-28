80d710f6543f82d0d79edf4ead17aa9c24437073
Tai <>
2019-07-28 13:07:47 +0900

    gas price reducer for each speed

--- src/reducers/ReducerGasPrice.js
+++ src/reducers/ReducerGasPrice.js
@@ -1,14 +1,22 @@
 'use strict';
-import { GET_GAS_PRICE } from '../constants/ActionTypes';
+import { GET_GAS_PRICE_FAST, GET_GAS_PRICE_AVERAGE, GET_GAS_PRICE_SLOW } from '../constants/ActionTypes';
 
 const INITIAL_STATE = {
-  gasPrice: {}
+  gasPrice: {
+    fast: '0',
+    average: '0',
+    slow: '0'
+  }
 };
 
 const gasPrice = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case GET_GAS_PRICE:
-      return { ...state, gasPrice: action.payload };
+    case GET_GAS_PRICE_FAST:
+      return { gasPrice: { ...state.gasPrice, fast: action.payload } };
+    case GET_GAS_PRICE_AVERAGE:
+      return { gasPrice: { ...state.gasPrice, average: action.payload } };
+    case GET_GAS_PRICE_SLOW:
+      return { gasPrice: { ...state.gasPrice, slow: action.payload } };
     default:
       return state || INITIAL_STATE;
   }
