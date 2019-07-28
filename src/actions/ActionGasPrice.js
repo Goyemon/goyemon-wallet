0577aa323a60a5293b74fdfc9dc700eb9ac8c294
Tai <>
2019-07-28 13:07:28 +0900

    gas price action for each speed

--- src/actions/ActionGasPrice.js
+++ src/actions/ActionGasPrice.js
@@ -1,19 +1,63 @@
 'use strict';
-import { GET_GAS_PRICE } from '../constants/ActionTypes';
+import { GET_GAS_PRICE_FAST, GET_GAS_PRICE_AVERAGE, GET_GAS_PRICE_SLOW } from '../constants/ActionTypes';
 import axios from 'axios';
+import ProviderController from '../wallet-core/ProviderController.ts';
 
-export function getGasPrice() {
+const WEB3 = ProviderController.setProvider();
+
+export function getGasPriceFast() {
   return async function (dispatch) {
     try {
       const gasPrice = await axios.get('https://ethgasstation.info/json/ethgasAPI.json');
-      dispatch(getGasPriceSuccess(gasPrice));
+      const gasPriceFastInGwei =  gasPrice.data.fastest.toString();
+      const gasPriceFastInWei = WEB3.utils.toWei(gasPriceFastInGwei, 'Gwei');
+      const gasPriceFastInEther = WEB3.utils.fromWei(gasPriceFastInWei, 'Ether');
+      dispatch(getGasPriceFastSuccess(gasPriceFastInEther));
     } catch(err) {
       console.error(err);
     }
   }
 };
 
-const getGasPriceSuccess = (gasPrice) => ({
-  type: GET_GAS_PRICE,
-  payload: gasPrice
+const getGasPriceFastSuccess = (gasPriceFastInEther) => ({
+  type: GET_GAS_PRICE_FAST,
+  payload: gasPriceFastInEther
+})
+
+export function getGasPriceAverage() {
+  return async function (dispatch) {
+    try {
+      const gasPrice = await axios.get('https://ethgasstation.info/json/ethgasAPI.json');
+      const gasPriceAverageInGwei =  gasPrice.data.fast.toString();
+      const gasPriceAverageInWei = WEB3.utils.toWei(gasPriceAverageInGwei, 'Gwei');
+      const gasPriceAverageInEther = WEB3.utils.fromWei( gasPriceAverageInWei, 'Ether');
+      dispatch(getGasPriceAverageSuccess(gasPriceAverageInEther));
+    } catch(err) {
+      console.error(err);
+    }
+  }
+};
+
+const getGasPriceAverageSuccess = (gasPriceAverageInEther) => ({
+  type: GET_GAS_PRICE_AVERAGE,
+  payload: gasPriceAverageInEther
+})
+
+export function getGasPriceSlow() {
+  return async function (dispatch) {
+    try {
+      const gasPrice = await axios.get('https://ethgasstation.info/json/ethgasAPI.json');
+      const gasPriceSlowInGwei =  gasPrice.data.average.toString();
+      const gasPriceSlowInWei = WEB3.utils.toWei(gasPriceSlowInGwei, 'Gwei');
+      const gasPriceSlowInEther = WEB3.utils.fromWei(gasPriceSlowInWei, 'Ether');
+      dispatch(getGasPriceSlowSuccess(gasPriceSlowInEther));
+    } catch(err) {
+      console.error(err);
+    }
+  }
+};
+
+const getGasPriceSlowSuccess = (gasPriceSlowInEther) => ({
+  type: GET_GAS_PRICE_SLOW,
+  payload: gasPriceSlowInEther
 })
