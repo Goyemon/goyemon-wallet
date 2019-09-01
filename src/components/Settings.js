d5619de744468699a34356134d02f76cca6e37f1
Tai <>
2019-09-01 07:37:19 +0200

    uninstall react-native-settings-list

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,17 +2,12 @@
 import React, { Component } from 'react';
 import { Text, Image } from 'react-native';
 import { RootContainer, HeaderOne } from '../components/common';
-import SettingsList from 'react-native-settings-list';
 
 export default class Settings extends Component {
   render() {
     return (
       <RootContainer>
         <HeaderOne  marginTop="96">Settings</HeaderOne>
-        <SettingsList borderColor="#c8c7cc" defaultItemSize={50}>
-          <SettingsList.Header headerStyle={{ marginTop: 16 }} />
-          <SettingsList.Header headerStyle={{ marginTop: 16 }} />
-        </SettingsList>
       </RootContainer>
     );
   }
