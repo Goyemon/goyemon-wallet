0b5cf1499dea09781947dacbb3d4be3913334987
Tai <>
2019-09-07 07:32:45 +0900

    delete price and balance properties from ReducerWallets

--- src/reducers/ReducerWallets.js
+++ src/reducers/ReducerWallets.js
@@ -1,41 +1,14 @@
 'use strict';
-import { GET_ETH_PRICE } from '../constants/ActionTypes';
-import { GET_DAI_PRICE } from '../constants/ActionTypes';
 
 const INITIAL_STATE = {
   wallets: [
-    { id: 0, coin: 'Ether', notation: 'ETH', price: 0, balance: 0 },
-    { id: 1, coin: 'Dai', notation: 'DAI', price: 0, balance: 0 }
+    { id: 0, coin: 'Ether', notation: 'ETH' },
+    { id: 1, coin: 'Dai', notation: 'DAI' }
   ]
 };
 
 const wallets = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case GET_ETH_PRICE:
-      const getEthWallets = state.wallets.map(wallet => {
-        if (wallet.id === 0) {
-          return { ...wallet, price: action.payload };
-        }
-        return wallet;
-      });
-      return {
-        ...state,
-        wallets: getEthWallets
-      };
-    case GET_DAI_PRICE:
-      const getDaiWallets = state.wallets.map(wallet => {
-        if (wallet.id === 1) {
-          return { ...wallet, price: action.payload };
-        }
-        return wallet;
-      });
-      return {
-        ...state,
-        wallets: getDaiWallets
-      };
-    default:
-      return state;
-  }
+  return state || INITIAL_STATE;
 };
 
 export default wallets;
