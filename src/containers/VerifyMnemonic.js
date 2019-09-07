dd546364e72813793a123b284148292a5e78c7e4
Tai <>
2019-09-07 18:18:51 +0900

    verify mnemonic words

--- src/containers/VerifyMnemonic.js
+++ src/containers/VerifyMnemonic.js
@@ -1,10 +1,9 @@
 'use strict';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { RootContainer, Button, HeaderOne } from '../components/common';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import { RootContainer, ProgressBar, Button, HeaderOne } from '../components/common';
 import { connect } from 'react-redux';
 import WalletController from '../wallet-core/WalletController.ts';
-import MnemonicWords from './MnemonicWords';
 import EthUtils from '../wallet-core/EthUtils.js';
 import { createChecksumAddress } from '../actions/ActionChecksumAddress';
 import { getEthPrice, getDaiPrice } from '../actions/ActionPrice';
@@ -13,6 +12,39 @@ import firebase from 'react-native-firebase';
 import uuidv4 from 'uuid/v4';
 
 class VerifyMnemonic extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mnemonicWords: [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+      ],
+      mnemonicWordsValidation: true
+    };
+  }
+
   async savePrivateKey() {
     const privateKey = await WalletController.createPrivateKey();
     await WalletController.setPrivateKey(privateKey);
@@ -33,7 +65,35 @@ class VerifyMnemonic extends Component {
     firebase.messaging().sendMessage(upstreamMessage);
   }
 
+  async validateForm() {
+    const mnemonicWords = this.state.mnemonicWords.join(" ");
+
+    if (WalletController.validateMnemonic(mnemonicWords) && (mnemonicWords === this.props.mnemonicWords)) {
+      this.setState({mnemonicWordsValidation: true});
+      await WalletController.setMnemonic(mnemonicWords);
+      await WalletController.generateWallet(mnemonicWords);
+      await this.savePrivateKey();
+      await this.props.createChecksumAddress();
+      await this.registerEthereumAddress();
+      await this.props.getEthPrice();
+      await this.props.getDaiPrice();
+      this.props.navigation.navigate('NotificationPermissionTutorial');
+    } else {
+      this.setState({mnemonicWordsValidation: false});
+      console.log('form validation failed!');
+    }
+  }
+
+  handleTextChange = (text, id) => {
+    const mnemonicWords = this.state.mnemonicWords;
+    mnemonicWords[id] = text;
+
+    this.setState({ mnemonicWords });
+  };
+
   render() {
+    const { mnemonicWords } = this.state;
+
     return (
       <RootContainer>
         <ProgressBar
@@ -44,21 +104,32 @@ class VerifyMnemonic extends Component {
           <HeaderOne marginTop="48">Create Wallet</HeaderOne>
         </View>
         <Container>
-          <MnemonicWords />
-          <Text>Did you really keep it safe already?</Text>
+          <Text>Please write down a list of words again.</Text>
+          <MnemonicWordsContainer style={styles.table}>
+            {this.state.mnemonicWords.map((word, id) => (
+              <View style={styles.cell} key={id}>
+                <MnemonicWordWrapper>
+                  <TextInput
+                    style={{ textAlign: 'center', padding: 4 }}
+                    placeholder={(id + 1).toString()}
+                    autoCapitalize="none"
+                    maxLength={15}
+                    onChangeText={text => {
+                      this.handleTextChange(text, id);
+                    }}
+                  />
+                </MnemonicWordWrapper>
+              </View>
+            ))}
+          </MnemonicWordsContainer>
           <Button
             text="Verify"
             textColor="white"
-            backgroundColor="#4083FF"
+            backgroundColor="#009DC4"
             margin="24px auto"
             opacity="1"
             onPress={async () => {
-              await this.savePrivateKey();
-              await this.props.createChecksumAddress();
-              await this.registerEthereumAddress();
-              await this.props.getEthPrice();
-              await this.props.getDaiPrice();
-              this.props.navigation.navigate('NotificationPermissionTutorial');
+              await this.validateForm();
             }}
           />
         </Container>
@@ -67,16 +138,42 @@ class VerifyMnemonic extends Component {
   }
 }
 
+const styles = StyleSheet.create({
+  table: {
+    flexWrap: 'wrap',
+    flexDirection: 'row'
+  },
+  cell: {
+    flexBasis: '25%',
+    flex: 1,
+    marginBottom: 8
+  }
+});
+
 const Container = styled.View`
   alignItems: center;
   flexDirection: column;
   justifyContent: center;
 `;
 
+const MnemonicWordsContainer = styled.View`
+  margin-bottom: 24px;
+  margin-top: 24px;
+  width: 95%;
+`;
+
+const MnemonicWordWrapper = styled.View`
+  background: #fff;
+  border-color: #f8f8f8;
+  border-radius: 16px;
+  border-width: 4px;
+  text-align: center;
+`;
+
 function mapStateToProps(state) {
   return {
-    web3: state.ReducerWeb3.web3,
-    checksumAddress: state.ReducerChecksumAddress.checksumAddress
+    checksumAddress: state.ReducerChecksumAddress.checksumAddress,
+    mnemonicWords: state.ReducerMnemonic.mnemonicWords
   };
 }
 
