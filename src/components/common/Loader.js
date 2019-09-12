15436930a8c34ec674defeb31d799e80239603a5
Tai <>
2019-09-12 12:50:00 +0900

    use the styled component in the Loader common component

--- src/components/common/Loader.js
+++ src/components/common/Loader.js
@@ -1,6 +1,7 @@
 'use strict';
 import React from 'react';
-import { Modal, View, ActivityIndicator } from 'react-native';
+import styled from 'styled-components';
+import { Modal, ActivityIndicator } from 'react-native';
 
 const Loader = props => {
   const { loading, ...attributes } = props;
@@ -10,33 +11,32 @@ const Loader = props => {
       transparent={true}
       animationType={'none'}
       visible={loading}>
-      <View style={styles.modalBackground}>
-        <View style={styles.activityIndicatorWrapper}>
+      <ModalBackground>
+        <ActivityIndicatorWrapper>
           <ActivityIndicator
             animating={loading} />
-        </View>
-      </View>
+        </ActivityIndicatorWrapper>
+      </ModalBackground>
     </Modal>
   )
 };
 
-const styles = {
-  modalBackground: {
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    backgroundColor: '#00000040'
-  },
-  activityIndicatorWrapper: {
-    backgroundColor: '#FFFFFF',
-    height: 100,
-    width: 100,
-    borderRadius: 10,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-around'
-  }
-};
+const ModalBackground = styled.View`
+  flex: 1;
+  alignItems: center;
+  flexDirection: column;
+  justifyContent: space-around;
+  backgroundColor: #00000040;
+`;
+
+const ActivityIndicatorWrapper = styled.View`
+  backgroundColor: #FFFFFF;
+  height: 120;
+  width: 120;
+  borderRadius: 16;
+  display: flex;
+  alignItems: center;
+  justifyContent: space-around;
+`;
 
 export { Loader };
