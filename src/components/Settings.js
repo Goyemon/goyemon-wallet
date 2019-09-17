a06090be19250703fa3c471bc95917b30a48151b
Tai <>
2019-09-17 15:45:09 +0900

    delete accounts in a modal window and reset a wallet

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,16 +1,59 @@
 'use strict';
 import React, { Component } from 'react';
 import { RootContainer, HeaderOne, UntouchableCardContainer, Button, OneLiner } from '../components/common';
+import { View, Text, Modal, TouchableHighlight, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import styled from 'styled-components/native';
 import WalletController from '../wallet-core/WalletController.ts';
 
 export default class Settings extends Component {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
   render() {
     return (
       <RootContainer>
-        <HeaderOne  marginTop="96">Settings</HeaderOne>
+        <HeaderOne marginTop="96">Settings</HeaderOne>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
             <OneLiner fontSize="24px" fontWeight="normal" marginBottom="0" marginLeft="0" marginTop="96">Delete Accounts?</OneLiner>
+            <ButtonContainer>
+              <Button
+                text="Cancel"
+                textColor="#5F5F5F"
+                backgroundColor="#EEEEEE"
+                margin="8px"
+                opacity="1"
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}
+              />
+              <Button
+                text="Confirm"
+                textColor="#FFF"
+                backgroundColor="#FF3346"
+                margin="8px"
+                opacity="1"
+                onPress={async () => {
+                  await WalletController.resetKeychainData();
+                  this.setModalVisible(!this.state.modalVisible);
+                  this.props.navigation.navigate('Initial');
+                }}
+              />
+            </ButtonContainer>
+          </View>
+        </Modal>
         <UntouchableCardContainer
           alignItems="flex-start"
           flexDirection="column"
@@ -19,44 +62,44 @@ export default class Settings extends Component {
           textAlign="left"
           width="95%"
          >
-         <SettingsTextContainer>
-           <Icon name="information-outline" color="#4E4E4E" size={32} />
-           <SettingsText>About Us</SettingsText>
-         </SettingsTextContainer>
-         <SettingsTextContainer>
-           <Icon name="message-text-outline" color="#4E4E4E" size={32} />
-           <SettingsText>Talk to Us</SettingsText>
-         </SettingsTextContainer>
-         <SettingsTextContainer>
-           <Button
-             text="Erase Keychain Data"
-             textColor="#4E4E4E"
-             backgroundColor="#EEEEEE"
-             margin="0"
-             opacity="1"
-             onPress={async () => {
-               await WalletController.resetKeychainData();
+           <SettingsTextContainer>
+             <Icon name="information-outline" color="#5F5F5F" size={32} />
+             <SettingsText>About Us</SettingsText>
+           </SettingsTextContainer>
            <SettingsTextContainer>
              <Icon name="message-text-outline" color="#5F5F5F" size={32} />
              <TouchableHighlight
+               underlayColor="#FFF"
                onPress={() => this.props.navigation.navigate('BackupWords')}
              >
                <SettingsText>Backup Words</SettingsText>
              </TouchableHighlight>
            </SettingsTextContainer>
-             }}
-           />
-         </SettingsTextContainer>
-        </UntouchableCardContainer>
+         </UntouchableCardContainer>
+         <UntouchableCardContainer
+           alignItems="center"
+           flexDirection="column"
+           height="80px"
+           justifyContent="center"
+           textAlign="center"
+           width="95%"
+          >
+             <TouchableHighlight
+               underlayColor="#FFF"
+               onPress={() => {
+                 this.setModalVisible(true);
+               }}
+             >
+               <DeleteAccountsText>Delete Accounts</DeleteAccountsText>
+             </TouchableHighlight>
+         </UntouchableCardContainer>
       </RootContainer>
     );
   }
 }
 
 const SettingsTextContainer = styled.View`
-  alignItems: center;
   flexDirection: row;
-  justifyContent: center;
   margin-bottom: 8px;
   margin-left: 24px;
   margin-top: 16px;
@@ -66,4 +109,16 @@ const SettingsText = styled.Text`
   color: #5F5F5F
   font-size: 24px;
   margin-left: 8px;
+`;
+
+const ButtonContainer = styled.View`
+  alignItems: center;
+  flexDirection: row;
+  justifyContent: center;
+  margin-top: 16px;
+`;
+
+const DeleteAccountsText = styled.Text`
+  color: #FF3346;
+  font-size: 24px;
 `;
