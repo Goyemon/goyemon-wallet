103a173cbeea2d0e724c92a80d701b3598eeb654
Tai <>
2019-10-05 18:30:00 +0900

    update the lottie-react-native package

--- ios/build/DeBank/Build/Intermediates.noindex/DeBank.build/Debug-iphonesimulator/DeBankTests.build/Objects-normal/x86_64/DeBankTests.d
+++ ios/build/DeBank/Build/Intermediates.noindex/DeBank.build/Debug-iphonesimulator/DeBankTests.build/Objects-normal/x86_64/DeBankTests.d
@@ -1,4 +1,6 @@
 dependencies: \
+  /Users/tai/Dropbox/Codes/DeBank/ios/build/DeBank/Build/Products/Debug-iphonesimulator/lottie-ios/Lottie.modulemap \
+  /Users/tai/Dropbox/Codes/DeBank/ios/build/DeBank/Build/Products/Debug-iphonesimulator/lottie-react-native/lottie_react_native.modulemap \
   /Users/tai/Dropbox/Codes/DeBank/ios/DeBankTests/DeBankTests.m \
   /Users/tai/Dropbox/Codes/DeBank/ios/Pods/Firebase/CoreOnly/Sources/module.modulemap \
   /Users/tai/Dropbox/Codes/DeBank/ios/build/DeBank/Build/Products/Debug-iphonesimulator/include/React/RCTLog.h \
