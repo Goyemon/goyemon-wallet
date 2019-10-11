d674cd8a1418f2b8a1e9be5068b8d2dab19ff657
Tai <>
2019-10-11 18:04:07 +0900

    rebuild ios

--- ios/build/DeBank/Build/Intermediates.noindex/DeBank.build/Debug-iphonesimulator/DeBank.build/DerivedSources/DeBank_vers.c
+++ ios/build/DeBank/Build/Intermediates.noindex/DeBank.build/Debug-iphonesimulator/DeBank.build/DerivedSources/DeBank_vers.c
@@ -1,5 +1,5 @@
  extern const unsigned char DeBankVersionString[];
  extern const double DeBankVersionNumber;
 
- const unsigned char DeBankVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DeBank  PROJECT:DeBank-1" "\n";
- const double DeBankVersionNumber __attribute__ ((used)) = (double)1.;
+ const unsigned char DeBankVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DeBank  PROJECT:DeBank-4" "\n";
+ const double DeBankVersionNumber __attribute__ ((used)) = (double)4.;
