d9b4e035c4d1cd2428f42d2f8f6a1ea56b22c07f
Tai <>
2019-10-12 12:15:19 +0900

    add the FixLibswiftCoreErrorFile

---
+++ ios/FixLibswiftCoreErrorFile.swift
@@ -0,0 +1,11 @@
+//
+//  FixLibswiftCoreErrorFile.swift
+//  DeBank
+//
+//  Created by Tai on 10/12/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+
+// https://stackoverflow.com/a/56176956/3195156
