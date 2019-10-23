22a732bced0a8365e14290b273be83609d50b3fc
Tai <>
2019-10-23 21:30:20 +0900

    change the ios bundle identifier and rebuild ios and android

--- ios/build/DeBank/Build/Intermediates.noindex/DeBank.build/Debug-iphonesimulator/DeBank.build/DerivedSources/DeBank_vers.c
+++ ios/build/DeBank/Build/Intermediates.noindex/DeBank.build/Debug-iphonesimulator/DeBank.build/DerivedSources/DeBank_vers.c
@@ -1,5 +1,5 @@
  extern const unsigned char DeBankVersionString[];
  extern const double DeBankVersionNumber;
 
- const unsigned char DeBankVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DeBank  PROJECT:DeBank-9" "\n";
- const double DeBankVersionNumber __attribute__ ((used)) = (double)9.;
+ const unsigned char DeBankVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DeBank  PROJECT:DeBank-13" "\n";
+ const double DeBankVersionNumber __attribute__ ((used)) = (double)13.;
