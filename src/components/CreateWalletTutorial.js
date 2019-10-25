5e2555390038af00de6ee45bc1e0128b471e2119
Tai <>
2019-10-25 21:44:20 +0900

    add the feature to skip the mnemonic words verification

--- src/components/CreateWalletTutorial.js
+++ src/components/CreateWalletTutorial.js
@@ -3,9 +3,37 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import styled from 'styled-components/native';
 import { RootContainer, Button, HeaderOne, OneLiner } from '../components/common';
+import { connect } from 'react-redux';
+import WalletUtilities from '../utilities/WalletUtilities.ts';
+import EtherUtilities from '../utilities/EtherUtilities.js';
+import { createChecksumAddress } from '../actions/ActionChecksumAddress';
+import firebase from 'react-native-firebase';
+import uuidv4 from 'uuid/v4';
 
-export default class CreateWalletTutorial extends Component {
+class CreateWalletTutorial extends Component {
+  async savePrivateKey() {
+    const privateKey = await WalletUtilities.createPrivateKey();
+    await WalletUtilities.setPrivateKey(privateKey);
+  }
+
+  async registerEthereumAddress() {
+    const messageId = uuidv4();
+    const serverAddress = '400937673843@gcm.googleapis.com';
+    const checksumAddressWithoutPrefix = EtherUtilities.stripHexPrefix(this.props.checksumAddress);
+
+    const upstreamMessage = new firebase.messaging.RemoteMessage()
+      .setMessageId(messageId)
+      .setTo(serverAddress)
+      .setData({
+        register: 'true',
+        address: checksumAddressWithoutPrefix
+      });
+    firebase.messaging().sendMessage(upstreamMessage);
+  }
+
   render() {
+    const mnemonicWords = this.props.mnemonicWords;
+    
     return (
       <RootContainer>
         <OneLiner fontSize="24px" fontWeight="bold" marginBottom="24" marginLeft="0" marginTop="96">
@@ -28,6 +56,21 @@ export default class CreateWalletTutorial extends Component {
             opacity="1"
             onPress={() => this.props.navigation.navigate('ShowMnemonic')}
           />
+          <Button
+            text="Do This Later"
+            textColor="#009DC4"
+            backgroundColor="#F8F8F8"
+            margin="24px auto"
+            opacity="1"
+            onPress={async () => {
+              await WalletUtilities.setMnemonic(mnemonicWords);
+              await WalletUtilities.generateWallet(mnemonicWords);
+              await this.savePrivateKey();
+              await this.props.createChecksumAddress();
+              await this.registerEthereumAddress();
+              this.props.navigation.navigate('NotificationPermissionTutorial')
+            }}
+          />
         </Container>
       </RootContainer>
     );
@@ -39,3 +82,19 @@ const Container = styled.View`
   flexDirection: column;
   justifyContent: center;
 `;
+
+function mapStateToProps(state) {
+  return {
+    checksumAddress: state.ReducerChecksumAddress.checksumAddress,
+    mnemonicWords: state.ReducerMnemonic.mnemonicWords
+  };
+}
+
+const mapDispatchToProps = {
+  createChecksumAddress
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateWalletTutorial);
