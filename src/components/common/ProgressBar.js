097fbeb27394324a57d37848f2c32bf08f598852
Tai <>
2019-11-08 18:18:37 +0900

    edit the common ProgressBar component

--- src/components/common/ProgressBar.js
+++ src/components/common/ProgressBar.js
@@ -5,31 +5,87 @@ import styled from 'styled-components';
 
 const ProgressBar = props => (
   <Container>
-    <Text>{props.text} of 3 steps</Text>
-    <Outer>
-      <Inner width={props.width} />
-    </Outer>
+    <Outer />
+    <InnerContainer marginRight={props.marginRight} width={props.width}><Inner /></InnerContainer>
+    <NumberContainer>
+      <OneContainer oneColor={props.oneColor}><One>1</One></OneContainer>
+      <TwoContainer twoColor={props.twoColor}r><Two>2</Two></TwoContainer>
+      <ThreeContainer threeColor={props.threeColor}><Three>3</Three></ThreeContainer>
+    </NumberContainer>
   </Container>
 );
 
 const Container = styled.View`
   alignItems: center;
-  margin-top: 96px;
+  margin-top: 112px;
 `;
 
 const Outer = styled.View`
   background-color: #eeeeee;
   border-radius: 16px;
-  height: 16px;
-  margin-top: 16px;
+  height: 8px;
   width: 80%;
 `;
 
-const Inner = styled.View`
+const InnerContainer = styled.View`
   background-color: #5f5f5f;
   border-radius: 16px;
-  height: 16px;
+  height: 8px;
+  margin-top: -8px;
+  margin-right: ${props => props.marginRight};
   width: ${props => props.width};
+`;
+
+const Inner = styled.View`
+  text-align: left;
+`;
+
+const NumberContainer = styled.View`
+  alignItems: center;
+  flex: 1;
+  flexDirection: row;
+  justifyContent: space-around;
+  margin-top: -16px;
+  width: 115%;
+`;
+
+const OneContainer = styled.View`
+  alignItems: center;
+  background-color: ${props => props.oneColor};
+  border-radius: 12px;
+  height: 24px;
+  justifyContent: center;
+  width: 24px;
+`;
+
+const One = styled.Text`
+  color: #FFF;
+`;
+
+const TwoContainer = styled.View`
+  alignItems: center;
+  background-color: ${props => props.twoColor};
+  border-radius: 12px;
+  height: 24px;
+  justifyContent: center;
+  width: 24px;
+`;
+
+const Two = styled.Text`
+  color: #FFF;
+`;
+
+const ThreeContainer = styled.View`
+  alignItems: center;
+  background-color: ${props => props.threeColor};
+  border-radius: 12px;
+  height: 24px;
+  justifyContent: center;
+  width: 24px;
+`;
+
+const Three = styled.Text`
+  color: #FFF;
 `;
 
 export { ProgressBar };
