7a1e535461c652c8fbacdb390db559f09f834ab1
Tai <>
2019-11-08 18:24:08 +0900

    style the Welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 'use strict';
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { RootContainer, Button } from '../components/common';
+import { RootContainer, Button, HeaderTwo } from '../components/common';
 import styled from 'styled-components/native';
 import * as Animatable from 'react-native-animatable';
 
@@ -10,26 +10,52 @@ export default class Welcome extends Component {
     return (
       <RootContainer>
         <Container>
-          <Title animation="fadeInDown" delay={500}>
-            <TitleRedText>h</TitleRedText>
-            <TitleGreenText>o</TitleGreenText>
-            <TitleOrangeText>ll</TitleOrangeText>
-            <TitleGreenText>a</TitleGreenText>
-            <TitleRedText>!</TitleRedText>
-          </Title>
-          <Title animation="fadeIn" delay={2000}>
-            ready to join?
-          </Title>
-          <ButtonWrapper animation="fadeIn" delay={4000}>
-            <Button
-              text="Hell Yeah!"
-              textColor="white"
-              backgroundColor="#009DC4"
-              margin="16px auto"
-              opacity="1"
-              onPress={() => this.props.navigation.navigate('Start')}
-            />
-          </ButtonWrapper>
+          <HollaContainer animation="fadeInDown" delay={500}>
+            <Title>
+              <TitleRedText>h</TitleRedText>
+              <TitleGreenText>o</TitleGreenText>
+              <TitleOrangeText>ll</TitleOrangeText>
+              <TitleGreenText>a</TitleGreenText>
+              <TitleRedText>!</TitleRedText>
+            </Title>
+          </HollaContainer>
+          <WelcomeContainer animation="fadeIn" delay={2000}>
+            <Title>
+              Welcome to
+            </Title>
+            <Logo>
+              Crypterest
+            </Logo>
+            <HeaderTwo
+              fontSize="24px"
+              fontWeight="bold"
+              marginBottom="24"
+              marginLeft="0"
+              marginTop="16"
+            >
+              share your crypto assets,
+            </HeaderTwo>
+            <HeaderTwo
+              fontSize="24px"
+              fontWeight="bold"
+              marginBottom="24"
+              marginLeft="0"
+              marginTop="0"
+            >
+              earn passive income
+            </HeaderTwo>
+            <ButtonWrapper>
+              <Button
+                text="Continue"
+                textColor="#009DC4"
+                backgroundColor="#FFF"
+                borderColor="#009DC4"
+                margin="16px auto"
+                opacity="1"
+                onPress={() => this.props.navigation.navigate('Start')}
+              />
+            </ButtonWrapper>
+          </WelcomeContainer>
         </Container>
       </RootContainer>
     );
@@ -38,12 +64,30 @@ export default class Welcome extends Component {
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
   margin-top: 240px;
   text-align: center;
 `;
 
+const HollaContainer = Animatable.createAnimatableComponent(styled.View`
+  flex: 1;
+  margin-top: 16px;
+  text-align: center;
+`);
+
+const WelcomeContainer = Animatable.createAnimatableComponent(styled.View`
+  alignItems: center;
+  flex: 1;
+  margin-top: 20px;
+`);
+
 const Title = Animatable.createAnimatableComponent(styled.Text`
+  font-size: 48px;
+  margin-bottom: 16px;
+  text-align: center;
+`);
+
+const Logo = Animatable.createAnimatableComponent(styled.Text`
+  color: #ff3346;
   font-size: 48px;
   margin-bottom: 16px;
   text-align: center;
@@ -63,5 +107,4 @@ const TitleOrangeText = styled.Text`
 
 const ButtonWrapper = Animatable.createAnimatableComponent(styled.View`
   font-size: 40px;
-  alignItems: center;
 `);
