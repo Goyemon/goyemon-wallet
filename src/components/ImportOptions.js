dd61d37545620b4d5c2d35d4e42dfca271c63d69
Tai <>
2019-11-08 18:20:46 +0900

    style the ImportOptions component

--- src/components/ImportOptions.js
+++ src/components/ImportOptions.js
@@ -2,22 +2,26 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Text } from 'react-native';
-import { RootContainer, ProgressBar, HeaderOne, HeaderThree, Button } from '../components/common/';
+import { RootContainer, ProgressBar, HeaderTwo, Description, Button } from '../components/common/';
 
 export default class ImportOptions extends Component {
   render() {
     return (
       <RootContainer>
-        <ProgressBar text="1" width="33%" />
+        <ProgressBar oneColor="#5f5f5f" twoColor="#eeeeee" threeColor="#eeeeee" marginRight="40%" width="0%" />
         <Container>
-          <HeaderThree color="#000" fontSize="20px" marginBottom="8" marginLeft="8" marginTop="16">
+          <HeaderTwo marginBottom="16" marginLeft="0" marginTop="24">
+            Backup Words Option
+          </HeaderTwo>
+          <Description marginBottom="8" marginLeft="8" marginTop="16">
             How many backup words do you have?
-          </HeaderThree>
+          </Description>
           <ButtonContainer>
             <Button
               text="12 Words"
-              textColor="white"
-              backgroundColor="#009DC4"
+              textColor="#009DC4"
+              backgroundColor="#FFF"
+              borderColor="#009DC4"
               margin="8px auto"
               opacity="1"
               onPress={async () => {
@@ -26,8 +30,9 @@ export default class ImportOptions extends Component {
             />
             <Button
               text="24 Words"
-              textColor="white"
-              backgroundColor="#009DC4"
+              textColor="#009DC4"
+              backgroundColor="#FFF"
+              borderColor="#009DC4"
               margin="8px auto"
               opacity="1"
               onPress={async () => {
