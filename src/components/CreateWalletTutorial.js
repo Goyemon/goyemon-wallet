1b1bd017ca4ed2e6d1520ff1effc7ca00a64fd0c
Tai <>
2019-11-08 18:20:18 +0900

    style the CreateWalletTutorial component

--- src/components/CreateWalletTutorial.js
+++ src/components/CreateWalletTutorial.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import styled from 'styled-components/native';
-import { RootContainer, Button, HeaderOne, OneLiner } from '../components/common';
+import { RootContainer, Button, HeaderTwo, Description } from '../components/common';
 import { connect } from 'react-redux';
 import WalletUtilities from '../utilities/WalletUtilities.ts';
 import EtherUtilities from '../utilities/EtherUtilities.js';
@@ -22,22 +22,23 @@ class CreateWalletTutorial extends Component {
 
     return (
       <RootContainer>
-        <OneLiner fontSize="24px" fontWeight="bold" marginBottom="24" marginLeft="0" marginTop="96">
-          No Backup, No Money
-        </OneLiner>
+        <HeaderTwo marginBottom="24" marginLeft="0" marginTop="120">
+          With Great Power Comes Great Responsibility
+        </HeaderTwo>
         <Container>
-          <Text>
+          <Description marginBottom="16" marginLeft="8" marginTop="16">
             Your backup words are the only way to restore your wallet if your phone is lost, stoken,
             broken or upgraded.
-          </Text>
-          <Text>
+          </Description>
+          <Description marginBottom="16" marginLeft="8" marginTop="16">
             We will show you a list of words to save in the next screen. We strongly recommend that
             you write them down on a piece of paper. Please keep it somewhere safe.
-          </Text>
+          </Description>
           <Button
             text="Write down backup words"
-            textColor="white"
-            backgroundColor="#009DC4"
+            textColor="#009DC4"
+            backgroundColor="#FFF"
+            borderColor="#009DC4"
             margin="24px auto"
             opacity="1"
             onPress={() => this.props.navigation.navigate('ShowMnemonic')}
@@ -46,6 +47,7 @@ class CreateWalletTutorial extends Component {
             text="Do This Later"
             textColor="#009DC4"
             backgroundColor="#F8F8F8"
+            borderColor="#F8F8F8"
             margin="0 auto"
             opacity="1"
             onPress={async () => {
