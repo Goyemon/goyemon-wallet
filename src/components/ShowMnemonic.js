005c19375ff5c5454071c20ff42b21cf8f3efc5d
Tai <>
2019-11-08 18:22:08 +0900

    style the ShowMnemonic component

--- src/components/ShowMnemonic.js
+++ src/components/ShowMnemonic.js
@@ -2,29 +2,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import styled from 'styled-components/native';
-import { RootContainer, ProgressBar, Button, HeaderOne, HeaderThree, OneLiner } from '../components/common';
+import { RootContainer, ProgressBar, Button, HeaderTwo, Description } from '../components/common';
 import ShowMnemonicWords from '../containers/ShowMnemonicWords';
 
 export default class ShowMnemonic extends Component {
   render() {
     return (
       <RootContainer>
-        <ProgressBar text="1" width="33%" />
-        <OneLiner fontSize="24px" fontWeight="bold" marginBottom="16" marginLeft="0" marginTop="24">
+        <ProgressBar oneColor="#5f5f5f" twoColor="#eeeeee" threeColor="#eeeeee" marginRight="40%" width="0%" />
+        <HeaderTwo marginBottom="16" marginLeft="0" marginTop="24">
           Backup Words
-        </OneLiner>
+        </HeaderTwo>
         <Container>
-          <HeaderThree color="#000" fontSize="20px" marginBottom="8" marginLeft="8" marginTop="16">
+          <Description marginBottom="8" marginLeft="8" marginTop="16">
             Please carefully write down these 24 backup words.
-          </HeaderThree>
+          </Description>
           <ShowMnemonicWords />
-          <HeaderThree color="#000" fontSize="20px" marginBottom="8" marginLeft="8" marginTop="16">
+          <Description marginBottom="8" marginLeft="8" marginTop="16">
             we will confirm this phrase on the next screen
-          </HeaderThree>
+          </Description>
           <Button
             text="I’ve written them down"
-            textColor="white"
-            backgroundColor="#009DC4"
+            textColor="#009DC4"
+            backgroundColor="#FFF"
+            borderColor="#009DC4"
             margin="24px auto"
             opacity="1"
             onPress={() => this.props.navigation.navigate('VerifyMnemonic')}
