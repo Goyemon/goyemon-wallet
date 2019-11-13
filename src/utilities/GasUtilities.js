49633ad38c4b7507bd0bcebf7428081281a7365d
Tai <>
2019-11-13 14:05:08 +0900

    change the data type in the getTransactionFeeEstimateInEther method

--- src/utilities/GasUtilities.js
+++ src/utilities/GasUtilities.js
@@ -4,7 +4,7 @@ const web3 = ProviderUtilities.setProvider();
 
 class GasUtilities {
   getTransactionFeeEstimateInEther(gasPriceInWei, gasLimit) {
-    const transactionFeeEstimateInWei = gasPriceInWei * gasLimit;
+    const transactionFeeEstimateInWei = parseFloat(gasPriceInWei) * gasLimit;
     const transactionFeeEstimateInEther = web3.utils.fromWei(
       transactionFeeEstimateInWei.toString(),
       'Ether'
