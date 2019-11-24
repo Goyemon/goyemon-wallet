1a029b6b4449a911a07a48075cf4373bcae49604
Tai <>
2019-11-24 15:47:33 +0900

    change the Discord invitation link

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -86,7 +86,7 @@ class Settings extends Component {
         <CommunityIconContainer>
           <CommunityIcon><Icon onPress={() => {Linking.openURL('https://twitter.com/taisuke_mino')}} name="twitter" color="#00aced" size={40} /></CommunityIcon>
           <CommunityIcon><Icon onPress={() => {Linking.openURL('https://github.com/taisukemino')}} name="github-circle" color="#333" size={40} /></CommunityIcon>
-          <CommunityIcon><Icon onPress={() => {Linking.openURL('https://discord.gg/mtbXB2')}} name="discord" color="#7289DA" size={40} /></CommunityIcon>
+          <CommunityIcon><Icon onPress={() => {Linking.openURL('https://discord.gg/MXGfnJG')}} name="discord" color="#7289DA" size={40} /></CommunityIcon>
         </CommunityIconContainer>
         <Description
           marginBottom="40"
