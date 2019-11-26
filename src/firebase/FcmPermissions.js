89aa56c138e758e5f0846d61933d1134d3e7e4bf
Tai <>
2019-11-26 17:34:27 +0900

    make a new file to check the fcm permissions

---
+++ src/firebase/FcmPermissions.js
@@ -0,0 +1,31 @@
+'use strict';
+import firebase from 'react-native-firebase';
+import { store } from '../store/store';
+import { saveNotificationPermission } from '../actions/ActionNotificationPermission';
+
+class FcmPermissions {
+  async checkFcmPermissions() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      console.log('user has permissions');
+      store.dispatch(saveNotificationPermission(true));
+    } else {
+      console.log("user doesn't have permission");
+      try {
+        await firebase.messaging().requestPermission();
+        const enabled = await firebase.messaging().hasPermission();
+        if (enabled) {
+          store.dispatch(saveNotificationPermission(true));
+          console.log('User has authorised');
+        } else {
+          store.dispatch(saveNotificationPermission(false));
+        }
+      } catch (error) {
+        console.log('User has rejected permissions');
+        store.dispatch(saveNotificationPermission(false));
+      }
+    }
+  }
+}
+
+export default new FcmPermissions();
