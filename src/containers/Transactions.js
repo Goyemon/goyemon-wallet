5e2d05a38e76318f01778d0e6bef2901b3107735
Tai <>
2019-11-28 16:23:23 +0900

    move the logic to update txes to FirebaseListener.js

--- src/containers/Transactions.js
+++ src/containers/Transactions.js
@@ -3,36 +3,9 @@ import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import Transaction from './Transaction';
-import {
-  addPendingTransaction,
-  updateTransactionState
-} from '../actions/ActionTransactionHistory';
 import styled from 'styled-components/native';
-import firebase from 'react-native-firebase';
 
 class Transactions extends Component {
-  componentDidMount() {
-    this.messageListener = firebase.messaging().onMessage(downstreamMessage => {
-      if (downstreamMessage.data.type === 'txstate' && downstreamMessage.data.state === 'pending') {
-        this.props.addPendingTransaction(downstreamMessage.data);
-      } else if (downstreamMessage.data.type === 'txstate' && downstreamMessage.data.state === 'included') {
-        this.props.transactions.map(transaction => {
-          if( transaction.hash === downstreamMessage.data.txhash ){
-            this.props.updateTransactionState(downstreamMessage.data);
-          }
-        });
-      } else if (downstreamMessage.data.type === 'txstate' && downstreamMessage.data.state === 'confirmed') {
-        this.props.transactions.map(transaction => {
-          if(transaction.hash === downstreamMessage.data.txhash){
-            this.props.updateTransactionState(downstreamMessage.data);
-          }
-        });
-      }
-    });
-  }
-
-  componentWillUnmount() {
-    this.messageListener();
   }
 
   renderTransactions(){
@@ -89,9 +62,4 @@ const mapStateToProps = state => ({
   transactions: state.ReducerTransactionHistory.transactions
 });
 
-const mapDispatchToProps = {
-  addPendingTransaction,
-  updateTransactionState
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Transactions);
+export default connect(mapStateToProps)(Transactions);
--- src/firebase/FirebaseListener.js
+++ src/firebase/FirebaseListener.js
@@ -6,11 +6,16 @@ import {
   saveEmptyTransaction,
   saveExistingTransactions
 } from '../actions/ActionTransactionHistory';
+import {
+  addPendingTransaction,
+  updateTransactionState
+} from '../actions/ActionTransactionHistory';
 
 store.dispatch(saveWeb3());
 
 const stateTree = store.getState();
 const web3 = stateTree.ReducerWeb3.web3;
+const transactionsHistory = stateTree.ReducerTransactionHistory.transactions;
 
 firebase.messaging().onMessage(downstreamMessage => {
   if (downstreamMessage.data.type === 'balance') {
@@ -23,5 +28,19 @@ firebase.messaging().onMessage(downstreamMessage => {
     store.dispatch(saveExistingTransactions(transactions));
   } else if (downstreamMessage.data.type === 'txhistory' && downstreamMessage.data.count === '0') {
     store.dispatch(saveEmptyTransaction(downstreamMessage.data.items));
+  } else if (downstreamMessage.data.type === 'txstate' && downstreamMessage.data.state === 'pending') {
+    store.dispatch(addPendingTransaction(downstreamMessage.data));
+  } else if (downstreamMessage.data.type === 'txstate' && downstreamMessage.data.state === 'included') {
+    transactionsHistory.map(transaction => {
+      if( transaction.hash === downstreamMessage.data.txhash ){
+        store.dispatch(updateTransactionState(downstreamMessage.data));
+      }
+    });
+  } else if (downstreamMessage.data.type === 'txstate' && downstreamMessage.data.state === 'confirmed') {
+    transactionsHistory.map(transaction => {
+      if(transaction.hash === downstreamMessage.data.txhash){
+        store.dispatch(updateTransactionState(downstreamMessage.data));
+      }
+    });
   }
 });
