369b6521ef628c958d5e01b8fab20a2bb10e1a4f
Tai <>
2019-12-02 13:41:56 +0900

    handle the null value in transactions props in the Transactions component

--- src/containers/Transactions.js
+++ src/containers/Transactions.js
@@ -6,38 +6,34 @@ import Transaction from './Transaction';
 import styled from 'styled-components/native';
 
 class Transactions extends Component {
-  }
+  // handleLoadMore = () => {};
 
-  renderTransactions(){
+  renderTransactions() {
     const { transactions } = this.props;
-    if(transactions.length > 0) {
-      return (
-        <FlatList
-          data={transactions}
-          renderItem={({item}) => (
-            <Transaction transaction={item} />
-          )}
-          keyExtractor={item => item.hash}
-        >
-        </FlatList>
-      )
-    } else {
+    if (transactions === null || transactions.length === 0) {
       return (
         <EmptyTransactionContainer>
           <EmptyTransactionEmoji>(°△°) b</EmptyTransactionEmoji>
           <EmptyTransactionText>holy tabula rasa!</EmptyTransactionText>
           <EmptyTransactionText> you don’t have any transactions yet!</EmptyTransactionText>
         </EmptyTransactionContainer>
-      )
+      );
+    } else if (transactions.length > 0) {
+      return (
+        <FlatList
+          data={transactions}
+          renderItem={({ item }) => <Transaction transaction={item} />}
+          keyExtractor={item => item.hash}
+          // onEndReached={this.handleLoadMore}
+          // onEndThreshold={1}
+          // initialNumToRender={5}
+        />
+      );
     }
   }
 
   render() {
-    return (
-      <View>
-        {this.renderTransactions()}
-      </View>
-    );
+    return <View>{this.renderTransactions()}</View>;
   }
 }
 
@@ -48,13 +44,13 @@ const EmptyTransactionContainer = styled.View`
 `;
 
 const EmptyTransactionEmoji = styled.Text`
-  color: #5F5F5F;
+  color: #5f5f5f;
   font-size: 40px;
   margin-bottom: 24px;
 `;
 
 const EmptyTransactionText = styled.Text`
-  color: #5F5F5F;
+  color: #5f5f5f;
   font-size: 16px;
 `;
 
