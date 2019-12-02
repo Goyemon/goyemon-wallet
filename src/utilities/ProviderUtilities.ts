a65d1d445fb80201a699a9ef05e289ab0e158b0e
Tai <>
2019-12-02 22:20:36 +0900

    remove the setProvider method from the ProviderUtilities

--- src/utilities/ProviderUtilities.ts
+++ src/utilities/ProviderUtilities.ts
@@ -1,20 +1,9 @@
 'use strict';
-import Web3 from 'web3';
 import firebase from 'react-native-firebase';
 import uuidv4 from 'uuid/v4';
 import EtherUtilities from './EtherUtilities.js';
 
 class ProviderUtilities {
-  private infuraId = '884958b4538343aaa814e3a32718ce91';
-
-  setProvider() {
-    const web3 = new Web3(
-      new Web3.providers.HttpProvider(`https://ropsten.infura.io/v3/${this.infuraId}`)
-    );
-
-    return web3;
-  }
-
   async registerEthereumAddress(checksumAddress) {
     const messageId = uuidv4();
     const serverAddress = '400937673843@gcm.googleapis.com';
