810e1b0fb387014204f7e56c06f5063b440a6e07
Tai <>
2019-12-06 23:18:49 +0900

    import react-native-gesture-handler to fix the crash

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 import './shim';
 import './base64-polyfill';
+import "react-native-gesture-handler";
 import { AppRegistry } from 'react-native';
 import App from './src/navigators/AppTab';
 import { name as appName } from './app.json';
