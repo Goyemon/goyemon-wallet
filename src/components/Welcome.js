f2a72c20850bf85a499e8c9432a44d613432aaea
Tai <>
2019-12-07 16:14:01 +0900

    style the Welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -79,15 +79,15 @@ export default class Welcome extends Component {
               alignItems="center"
               flexDirection="row"
               height="120px"
-              justifyContent="center"
+              justifyContent="space-between"
               textAlign="left"
-              width="90%"
+              width="80%"
               onPress={() => {
                 this.props.navigation.navigate('CreateWalletTutorial');
               }}
             >
               <View>
-                <HeaderThree marginBottom="0" marginLeft="0" marginTop="0">
+                <HeaderThree color="#00A3E2" marginBottom="0" marginLeft="8" marginTop="0">
                   Create
                 </HeaderThree>
                 <CardText>new wallet</CardText>
@@ -100,13 +100,13 @@ export default class Welcome extends Component {
               alignItems="center"
               flexDirection="row"
               height="120px"
-              justifyContent="center"
+              justifyContent="space-between"
               textAlign="left"
-              width="90%"
+              width="80%"
               onPress={() => this.props.navigation.navigate('ImportOptions')}
             >
               <View>
-                <HeaderThree marginBottom="0" marginLeft="0" marginTop="0">
+                <HeaderThree color="#00A3E2" marginBottom="0" marginLeft="8" marginTop="0">
                   Import
                 </HeaderThree>
                 <CardText>existing wallet</CardText>
@@ -172,6 +172,7 @@ const TitleOrangeText = styled.Text`
 const CardText = styled.Text`
   font-family: 'HKGrotesk-Regular';
   font-size: 16px;
+  margin-left: 8px;
   padding-top: 8px;
   padding-right: 32px;
   text-align: left;
@@ -179,6 +180,7 @@ const CardText = styled.Text`
 
 const CardImage = styled.Image`
   height: 64px;
+  margin-right: 8px;
   padding: 16px;
   resizeMode: contain;
   width: 64px;
