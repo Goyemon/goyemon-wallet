2b10148c7d89ee4b60169f155dfc9bc709412fe9
Tai <>
2019-12-12 08:56:39 +0900

    rebuild ios and android

--- ios/build/DeBank/Build/Intermediates.noindex/debank.build/Debug-iphonesimulator/DeBank.build/DerivedSources/Crypterest_vers.c
+++ ios/build/DeBank/Build/Intermediates.noindex/debank.build/Debug-iphonesimulator/DeBank.build/DerivedSources/Crypterest_vers.c
@@ -1,5 +1,5 @@
  extern const unsigned char CrypterestVersionString[];
  extern const double CrypterestVersionNumber;
 
- const unsigned char CrypterestVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Crypterest  PROJECT:DeBank-13" "\n";
- const double CrypterestVersionNumber __attribute__ ((used)) = (double)13.;
+ const unsigned char CrypterestVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Crypterest  PROJECT:DeBank-15" "\n";
+ const double CrypterestVersionNumber __attribute__ ((used)) = (double)15.;
