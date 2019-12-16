4a66f8f68cde38239ceb1037cc2b9d67a18a539a
Tai <>
2019-12-16 11:02:54 +0900

    import the firebaseListener function and pass it to the registerHeadlessTask for backgroundprocessing

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from './src/navigators/AppTab';
 import { name as appName } from './app.json';
+import { firebaseListener } from './src/firebase/FirebaseListener';
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => firebaseListener);
