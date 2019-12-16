dbccb0df70b68e364ee33a19876c33d9be72855b
Tai <>
2019-12-16 11:03:50 +0900

    format the code in FirebaseListener.js

--- src/firebase/FirebaseListener.js
+++ src/firebase/FirebaseListener.js
@@ -10,45 +10,52 @@ import { addPendingTransaction, updateTransactionState } from '../actions/Action
 import Web3 from 'web3';
 
 const firebaseListener = async () => {
-firebase.messaging().onMessage(downstreamMessage => {
-  const stateTree = store.getState();
-  const transactionsHistory = stateTree.ReducerTransactionHistory.transactions;
-  if (downstreamMessage.data.type === 'balance') {
-    if (downstreamMessage.data.hasOwnProperty('eth')) {
-      const ethBalanceInWei = parseInt(downstreamMessage.data.eth, 16);
-      const balanceInEther = Web3.utils.fromWei(ethBalanceInWei.toString());
-      const roundedBalanceInEther = parseFloat(balanceInEther).toFixed(4);
-      store.dispatch(saveEthBalance(roundedBalanceInEther));
+  firebase.messaging().onMessage(downstreamMessage => {
+    console.log('downstreamMessage ===>', downstreamMessage);
+    const stateTree = store.getState();
+    const transactionsHistory = stateTree.ReducerTransactionHistory.transactions;
+    if (downstreamMessage.data.type === 'balance') {
+      if (downstreamMessage.data.hasOwnProperty('eth')) {
+        const ethBalanceInWei = parseInt(downstreamMessage.data.eth, 16);
+        const balanceInEther = Web3.utils.fromWei(ethBalanceInWei.toString());
+        const roundedBalanceInEther = parseFloat(balanceInEther).toFixed(4);
+        store.dispatch(saveEthBalance(roundedBalanceInEther));
+      }
+      if (downstreamMessage.data.hasOwnProperty('ame_ropsten')) {
+        let daiBalance = parseInt(downstreamMessage.data.ame_ropsten, 16) / 10 ** 18;
+        daiBalance = daiBalance.toFixed(2);
+        store.dispatch(saveDaiBalance(daiBalance));
+      }
+    } else if (downstreamMessage.data.type === 'txhistory' && downstreamMessage.data.count != '0') {
+      const transactions = JSON.parse(downstreamMessage.data.items);
+      store.dispatch(saveExistingTransactions(transactions));
+    } else if (
+      downstreamMessage.data.type === 'txhistory' &&
+      downstreamMessage.data.count === '0'
+    ) {
+      store.dispatch(saveEmptyTransaction(downstreamMessage.data.items));
+    } else if (downstreamMessage.data.type === 'txstate') {
+      if (Array.isArray(transactionsHistory)) {
+        transactionsHistory.map(transaction => {
+          if (
+            transaction.nonce === downstreamMessage.data.nonce &&
+            downstreamMessage.data.state === 'pending'
+          ) {
+            store.dispatch(addPendingTransaction(downstreamMessage.data));
+          } else if (
+            (transaction.hash === downstreamMessage.data.txhash &&
+              downstreamMessage.data.state === 'included') ||
+            (transaction.hash === downstreamMessage.data.txhash &&
+              downstreamMessage.data.state === 'confirmed')
+          ) {
+            store.dispatch(updateTransactionState(downstreamMessage.data));
+          }
+        });
+      } else if (transactionsHistory === null) {
+        console.log('transactions are null');
+      }
     }
-    if (downstreamMessage.data.hasOwnProperty('ame_ropsten')) {
-      let daiBalance = parseInt(downstreamMessage.data.ame_ropsten, 16) / 10 ** 18;
-      daiBalance = daiBalance.toFixed(2);
-      store.dispatch(saveDaiBalance(daiBalance));
-    }
-  } else if (downstreamMessage.data.type === 'txhistory' && downstreamMessage.data.count != '0') {
-    const transactions = JSON.parse(downstreamMessage.data.items);
-    store.dispatch(saveExistingTransactions(transactions));
-  } else if (downstreamMessage.data.type === 'txhistory' && downstreamMessage.data.count === '0') {
-    store.dispatch(saveEmptyTransaction(downstreamMessage.data.items));
-  } else if (downstreamMessage.data.type === 'txstate') {
-    if (Array.isArray(transactionsHistory)) {
-      transactionsHistory.map(transaction => {
-        if (
-          transaction.nonce === downstreamMessage.data.nonce &&
-          downstreamMessage.data.state === 'pending'
-        ) {
-          store.dispatch(addPendingTransaction(downstreamMessage.data));
-        } else if (
-          (transaction.hash === downstreamMessage.data.txhash && downstreamMessage.data.state === 'included') || (transaction.hash === downstreamMessage.data.txhash && downstreamMessage.data.state === 'confirmed')
-        ) {
-          store.dispatch(updateTransactionState(downstreamMessage.data));
-        }
-      });
-    } else if (transactionsHistory === null) {
-      console.log('transactions are null');
-    }
-  }
-});
+  });
   return Promise.resolve();
 };
 
