7806b7b6a1ac335e6e50245683b9fcf424274664
Tai <>
2019-12-25 17:04:55 +0900

    rebuild ios and android

--- ios/build/Crypterest/Build/Intermediates.noindex/Crypterest.build/Debug-iphonesimulator/Crypterest.build/DerivedSources/Crypterest_vers.c
+++ ios/build/Crypterest/Build/Intermediates.noindex/Crypterest.build/Debug-iphonesimulator/Crypterest.build/DerivedSources/Crypterest_vers.c
@@ -1,5 +1,5 @@
  extern const unsigned char CrypterestVersionString[];
  extern const double CrypterestVersionNumber;
 
- const unsigned char CrypterestVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Crypterest  PROJECT:Crypterest-20" "\n";
- const double CrypterestVersionNumber __attribute__ ((used)) = (double)20.;
+ const unsigned char CrypterestVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Crypterest  PROJECT:Crypterest-24" "\n";
+ const double CrypterestVersionNumber __attribute__ ((used)) = (double)24.;
