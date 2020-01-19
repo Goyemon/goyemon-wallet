bd7488333d187b04f325a00201a4e8f9fe43df6b
Tai <>
2020-01-19 16:16:51 +0900

    change the gasLimit of the mint method tx

--- src/containers/SupplyDai.js
+++ src/containers/SupplyDai.js
@@ -108,7 +108,7 @@ class SupplyDai extends Component {
       nonce: `0x${transactionNonce.toString(16)}`,
       to: cDaiContract.cDaiAddress,
       gasPrice: `0x${this.state.gasPrice[this.state.checked].gasPriceWei.toString(16)}`,
-      gasLimit: `0x${parseFloat(100000).toString(16)}`,
+      gasLimit: `0x${parseFloat(250000).toString(16)}`,
       chainId: 3,
       data: mintEncodedABI
     };
