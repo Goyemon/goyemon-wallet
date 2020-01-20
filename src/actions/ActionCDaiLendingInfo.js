37c21b867645019f91fac5bcbdb81c39e82fffa5
Tai <>
2020-01-20 10:37:24 +0900

    add a new action that saves approval information of dai

--- src/actions/ActionCDaiLendingInfo.js
+++ src/actions/ActionCDaiLendingInfo.js
@@ -1,5 +1,5 @@
 'use strict';
-import { SAVE_CDAI_LENDING_INFO } from '../constants/ActionTypes';
+import { SAVE_CDAI_LENDING_INFO, SAVE_DAI_APPROVAL_INFO } from '../constants/ActionTypes';
 
 export function saveCDaiLendingInfo(cDaiLendingInfo) {
   return async function (dispatch) {
@@ -14,4 +14,19 @@ export function saveCDaiLendingInfo(cDaiLendingInfo) {
 const saveCDaiLendingInfoSuccess = (cDaiLendingInfo) => ({
   type: SAVE_CDAI_LENDING_INFO,
   payload: cDaiLendingInfo
+})
+
+export function saveDaiApprovalInfo(daiApprovalInfo) {
+  return async function (dispatch) {
+    try {
+      dispatch(saveDaiApprovalInfoSuccess(daiApprovalInfo));
+    } catch(err) {
+      console.error(err);
+    }
+  }
+};
+
+const saveDaiApprovalInfoSuccess = (daiApprovalInfo) => ({
+  type: SAVE_DAI_APPROVAL_INFO,
+  payload: daiApprovalInfo
 })
