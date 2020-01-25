eea17544f468f9ca4aa1bc9d4d31cb3bc202bfa8
Tai <4382091+taisukemino@users.noreply.github.com>
2020-01-25 13:45:50 +0900

    remove the 'Remote debugger' warning

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 import './shim';
 import './base64-polyfill';
+import { YellowBox } from 'react-native';
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from './src/navigators/AppTab';
 import { name as appName } from './app.json';
 import FcmBackgroundListener from './src/firebase/FcmBackgroundListener';
+
+YellowBox.ignoreWarnings(['Remote debugger']);
 
 AppRegistry.registerComponent(appName, () => App);
 AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => FcmBackgroundListener);
