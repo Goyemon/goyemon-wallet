111477fddd2c17560c56694ff8e44fee3aee25ad
Tai <4382091+taisukemino@users.noreply.github.com>
2020-01-27 09:49:51 +0900

    add a new action to save other debugging info

--- src/actions/ActionDebugInfo.js
+++ src/actions/ActionDebugInfo.js
@@ -1,5 +1,6 @@
 'use strict';
 import { SAVE_FCM_TOKEN } from '../constants/ActionTypes';
+import { SAVE_OTHER_DEBUG_INFO } from '../constants/ActionTypes';
 
 export function saveFcmToken(fcmToken) {
   return async function (dispatch) {
@@ -14,4 +15,19 @@ export function saveFcmToken(fcmToken) {
 const saveFcmTokenSuccess = (fcmToken) => ({
   type: SAVE_FCM_TOKEN,
   payload: fcmToken
+})
+
+export function saveOtherDebugInfo(otherDebugInfo) {
+  return async function (dispatch) {
+    try {
+      dispatch(saveOtherDebugInfoSuccess(otherDebugInfo));
+    } catch(err) {
+      console.error(err);
+    }
+  }
+};
+
+const saveOtherDebugInfoSuccess = (otherDebugInfo) => ({
+  type: SAVE_OTHER_DEBUG_INFO,
+  payload: otherDebugInfo
 })
