e08e72029138e13d29a982aa81670dd5266fcb22
Tai <4382091+taisukemino@users.noreply.github.com>
2020-01-28 15:17:41 +0900

    navigate to EarnDai after the mint and redeemUnderlying tx to avoid extra cdai lending info request

--- src/containers/WithdrawDaiConfirmation.js
+++ src/containers/WithdrawDaiConfirmation.js
@@ -30,7 +30,7 @@ class WithdrawDaiConfirmation extends Component {
     ];
     await TransactionUtilities.sendOutgoingTransactionToServer(outgoingTransactionObject);
     this.props.saveOutgoingDaiTransactionAmount(this.props.outgoingDaiTransactionData.amount);
-    this.props.navigation.reset([NavigationActions.navigate({ routeName: 'EarnList' })], 0);
+    this.props.navigation.reset([NavigationActions.navigate({ routeName: 'EarnDai' })], 0);
   }
 
   toggleCurrencySymbol() {
