69003c17028cd174eb3e3eeb22e85e443a7a1a63
Tai <4382091+taisukemino@users.noreply.github.com>
2020-01-29 16:59:55 +0900

    integrate the ApproveDai component code with the EarnDai component

--- src/containers/EarnDai.js
+++ src/containers/EarnDai.js
@@ -2,25 +2,126 @@
 import BigNumber from 'bignumber.js';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text } from 'react-native';
+import { Modal, Alert, View, Text } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import styled from 'styled-components';
+import Web3 from 'web3';
+import { saveDaiApprovalInfo } from '../actions/ActionCDaiLendingInfo';
+import { getGasPriceAverage } from '../actions/ActionGasPrice';
+import { saveOutgoingDaiTransactionApproveAmount } from '../actions/ActionOutgoingDaiTransactionData';
 import {
   RootContainer,
   UntouchableCardContainer,
   TransactionButton,
   HeaderOne,
   HeaderFour,
-  Button
+  Button,
+  Description
 } from '../components/common/';
-import { saveDaiApprovalInfo } from '../actions/ActionCDaiLendingInfo';
+import cDaiContract from '../contracts/cDaiContract';
+import daiTokenContract from '../contracts/daiTokenContract';
+import GasUtilities from '../utilities/GasUtilities.js';
+import PriceUtilities from '../utilities/PriceUtilities.js';
 import TransactionUtilities from '../utilities/TransactionUtilities.ts';
+import Web3ProviderUtilities from '../utilities/Web3ProviderUtilities.js';
 
 class EarnDai extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+      ethAmountValidation: undefined
+    };
+    this.web3 = Web3ProviderUtilities.web3Provider();
+    this.ethBalance = Web3.utils.fromWei(props.balance.weiBalance.toString());
+  }
+
   async componentDidMount() {
     if (this.props.transactions != null && this.props.transactions.length != null) {
       this.props.saveDaiApprovalInfo(TransactionUtilities.daiApproved(this.props.transactions));
     }
+    this.props.getGasPriceAverage();
+  }
+
+  getTransactionFeeEstimateInUsd() {
+    let transactionFeeEstimateInUsd = PriceUtilities.convertEthToUsd(
+      GasUtilities.getTransactionFeeEstimateInEther(this.props.gasPrice.average, 50000)
+    );
+    transactionFeeEstimateInUsd = transactionFeeEstimateInUsd.toFixed(3);
+    return transactionFeeEstimateInUsd;
+  }
+
+  validateEthAmount() {
+    const transactionFeeLimitInEther = GasUtilities.getTransactionFeeEstimateInEther(
+      this.props.gasPrice.average,
+      50000
+    );
+
+    if (parseFloat(this.ethBalance) > parseFloat(transactionFeeLimitInEther)) {
+      console.log('the eth amount validated!');
+      this.setState({ ethAmountValidation: true });
+      return true;
+    }
+    console.log('wrong eth balance!');
+    this.setState({ ethAmountValidation: false });
+    return false;
+  }
+
+  renderInsufficientEthBalanceMessage() {
+    if (this.state.ethAmountValidation || this.state.ethAmountValidation === undefined) {
+      <Text>{this.getTransactionFeeEstimateInUsd()}</Text>;
+    } else {
+      return <ErrorMessage>you don't have enough ether 😟</ErrorMessage>;
+    }
+  }
+
+  getApproveEncodedABI() {
+    const daiTokenContractInstance = new this.web3.eth.Contract(
+      JSON.parse(daiTokenContract.daiTokenAbi),
+      daiTokenContract.daiTokenAddress
+    );
+
+    const addressSpender = cDaiContract.cDaiAddress;
+    const amount = this.web3.utils.toHex(-1);
+
+    const approveEncodedABI = daiTokenContractInstance.methods
+      .approve(addressSpender, amount)
+      .encodeABI();
+
+    return approveEncodedABI;
+  }
+
+  async constructTransactionObject() {
+    const transactionNonce = parseInt(TransactionUtilities.getTransactionNonce());
+    const approveEncodedABI = this.getApproveEncodedABI();
+    const transactionObject = {
+      nonce: `0x${transactionNonce.toString(16)}`,
+      to: daiTokenContract.daiTokenAddress,
+      gasPrice: `0x${this.props.gasPrice.average.toString(16)}`,
+      gasLimit: `0x${parseFloat(50000).toString(16)}`,
+      chainId: 3,
+      data: approveEncodedABI
+    };
+    return transactionObject;
+  }
+
+  sendTransaction = async () => {
+    const ethAmountValidation = this.validateEthAmount();
+
+    if (ethAmountValidation) {
+      console.log('validation successful');
+      const transactionObject = await this.constructTransactionObject();
+      await TransactionUtilities.sendOutgoingTransactionToServer(transactionObject);
+      this.props.saveOutgoingDaiTransactionApproveAmount(this.web3.utils.toHex(-1));
+      this.setModalVisible(false);
+      this.props.navigation.navigate('Dai');
+    } else {
+      console.log('validation failed!');
+    }
+  };
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
   }
 
   renderTransactionButtons() {
@@ -63,14 +164,14 @@ class EarnDai extends Component {
     if (!this.props.cDaiLendingInfo.daiApproval) {
       return (
         <Button
-          text="Approve"
+          text="Unlock Dai"
           textColor="#00A3E2"
           backgroundColor="#FFF"
           borderColor="#00A3E2"
           margin="40px auto"
           opacity="1"
           onPress={async () => {
-            this.props.navigation.navigate('ApproveDai');
+            this.setModalVisible(true);
           }}
         />
       );
@@ -87,6 +188,51 @@ class EarnDai extends Component {
     return (
       <RootContainer>
         <HeaderOne marginTop="96">Dai</HeaderOne>
+        <Modal
+          animationType="fade"
+          transparent
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}
+        >
+          <ModalContainer>
+            <ModalBackground>
+              <MondalInner>
+                <ModalTextContainer>
+                  <Description marginBottom="8" marginLeft="0" marginTop="16">
+                    There will be about $0.06 to unlock Dai.
+                  </Description>
+                </ModalTextContainer>
+                <ButtonContainer>
+                  <Button
+                    text="Cancel"
+                    textColor="#5F5F5F"
+                    backgroundColor="#EEEEEE"
+                    borderColor="#EEEEEE"
+                    margin="8px"
+                    opacity="1"
+                    onPress={() => {
+                      this.setModalVisible(false);
+                    }}
+                  />
+                  <Button
+                    text="Confirm"
+                    textColor="#00A3E2"
+                    backgroundColor="#FFF"
+                    borderColor="#00A3E2"
+                    margin="8px"
+                    opacity="1"
+                    onPress={async () => {
+                      await this.sendTransaction();
+                    }}
+                  />
+                </ButtonContainer>
+                <View>{this.renderInsufficientEthBalanceMessage()}</View>
+              </MondalInner>
+            </ModalBackground>
+          </ModalContainer>
+        </Modal>
         <UntouchableCardContainer
           alignItems="center"
           borderRadius="8"
@@ -110,6 +256,43 @@ class EarnDai extends Component {
     );
   }
 }
+
+const ModalContainer = styled.View`
+  background-color: rgba(0, 0, 0, 0.5);
+  flex-direction: row;
+  justify-content: center;
+  height: 100%;
+`;
+
+const ModalBackground = styled.View`
+  background-color: #fff;
+  border-radius: 16px;
+  border-top-width: 2;
+  border-top-color: #e41b13;
+  height: 30%;
+  min-height: 280px;
+  margin-top: 200;
+  width: 90%;
+`;
+
+const MondalInner = styled.View`
+  justify-content: center;
+  flex: 1;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const ModalTextContainer = styled.View`
+  margin: 0 auto;
+  width: 90%;
+`;
+
+const ErrorMessage = styled.Text`
+  color: #e41b13;
+  font-family: 'HKGrotesk-Regular';
+  text-align: center;
+  width: 100%;
+`;
 
 const BalanceText = styled.Text`
   color: #000;
@@ -140,13 +323,16 @@ const ButtonContainer = styled.View`
 function mapStateToProps(state) {
   return {
     balance: state.ReducerBalance.balance,
+    gasPrice: state.ReducerGasPrice.gasPrice,
     cDaiLendingInfo: state.ReducerCDaiLendingInfo.cDaiLendingInfo,
     transactions: state.ReducerTransactionHistory.transactions
   };
 }
 
 const mapDispatchToProps = {
-  saveDaiApprovalInfo
+  getGasPriceAverage,
+  saveDaiApprovalInfo,
+  saveOutgoingDaiTransactionApproveAmount
 };
 
 export default withNavigation(
