fc0b48ee8aa16f701ae7cb796623d977cb56a2b1
Tai <4382091+taisukemino@users.noreply.github.com>
2020-02-03 10:40:00 +0900

    navigate to the scan screen in the eth sending screen

--- src/containers/SendEth.js
+++ src/containers/SendEth.js
@@ -250,6 +250,13 @@ class SendEth extends Component {
                   this.setState({ toAddress });
                 }}
               />
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.navigation.navigate('QRCodeScan');
+                }}
+              >
+                <Icon name="qrcode-scan" size={16} color="#5f5f5f" />
+              </TouchableOpacity>
             </SendTextInputContainer>
           </Form>
           <View>{this.renderInvalidToAddressMessage()}</View>
