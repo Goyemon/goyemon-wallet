c0e779b63754c3edc421bf3dba09379e57624c47
Tai <4382091+taisukemino@users.noreply.github.com>
2020-02-06 13:08:49 +0900

    move back to the sending screen from the scanning screen on scanning success or manually

--- src/components/QRCodeScan.js
+++ src/components/QRCodeScan.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, TouchableOpacity, Text } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { NavigationActions } from 'react-navigation';
+import { connect } from 'react-redux';
 import styled from 'styled-components/native';
+import { saveQRCodeData } from '../actions/ActionQRCodeData';
 import HomeStack from '../navigators/HomeStack';
 
 class QRCodeScan extends Component {
-  componentDidMount() {
+  onScanSuccess = e => {
+    this.props.saveQRCodeData(e.data);
+    this.props.navigation.pop();
+
     HomeStack.navigationOptions = () => {
-      const tabBarVisible = false;
+      const tabBarVisible = true;
       return {
         tabBarVisible
       };
     };
-  }
-
-  onScanSuccess = async e => {
-    await this.props.navigation.navigate('QRCodeData', {
-      data: e.data,
-      scanner: this.scanner
-    });
   };
 
   render() {
@@ -29,10 +27,22 @@ class QRCodeScan extends Component {
           onRead={this.onScanSuccess}
           showMarker
           checkAndroid6Permissions
-          ref={node => {
-            this.scanner = node;
-          }}
           cameraStyle={{ height: Dimensions.get('screen').height }}
+          bottomContent={
+            <GoBackContainer
+              onPress={() => {
+                HomeStack.navigationOptions = () => {
+                  const tabBarVisible = true;
+                  return {
+                    tabBarVisible
+                  };
+                };
+                this.props.navigation.goBack();
+              }}
+            >
+              <GoBackText>go back</GoBackText>
+            </GoBackContainer>
+          }
         />
       </Container>
     );
@@ -45,4 +55,21 @@ const Container = styled.View`
   align-items: center;
 `;
 
-export default QRCodeScan;
+const GoBackContainer = styled.TouchableOpacity`
+  align-items: center;
+  min-height: 120;
+  width: 100%;
+`;
+
+const GoBackText = styled.Text`
+  color: #fff;
+`;
+
+const mapDispatchToProps = {
+  saveQRCodeData
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(QRCodeScan);
