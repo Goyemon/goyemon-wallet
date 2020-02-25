a26db45255dafcb3a3f04c4a0218218c1b88bccc
Tai <4382091+taisukemino@users.noreply.github.com>
2020-02-25 20:58:35 +0900

    rename an object and property names in the ReducerWallets

--- src/reducers/ReducerWallets.js
+++ src/reducers/ReducerWallets.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const INITIAL_STATE = {
-  wallets: [{ id: 0, coin: 'Ether', notation: 'ETH' }, { id: 1, coin: 'Dai', notation: 'DAI' }]
+  currencies: [{ id: 0, name: 'Ether', notation: 'ETH' }, { id: 1, name: 'Dai', notation: 'DAI' }]
 };
 
-const wallets = (state = INITIAL_STATE, action) => state || INITIAL_STATE;
+const currencies = (state = INITIAL_STATE, action) => state || INITIAL_STATE;
 
-export default wallets;
+export default currencies;
