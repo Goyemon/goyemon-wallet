ba9ec5d7c37803dd9b56b5de5a12959b25406613
ruru <ruru@lain.pl>
2020-02-25 18:24:57 +0900

    deduplicated code

--- src/firebase/FcmUpstreamMsgs.ts
+++ src/firebase/FcmUpstreamMsgs.ts
@@ -2,50 +2,33 @@
 import firebase from 'react-native-firebase';
 import uuidv4 from 'uuid/v4';
 import EtherUtilities from '../utilities/EtherUtilities.js';
+import GlobalConfig from '../config.json';
 
 class FcmUpstreamMsgs {
-  constructor() {
-    this.messageId = uuidv4();
-    this.serverAddress = '255097673919@gcm.googleapis.com';
+  __sendMessage(type, checksumAddress) {
+    const checksumAddressWithoutPrefix = EtherUtilities.stripHexPrefix(checksumAddress);
+
+    const upstreamMessage = new firebase.messaging.RemoteMessage()
+      .setMessageId(uuidv4())
+      .setTo(GlobalConfig.FCM_server_address)
+      .setData({
+        type: type,
+        address: checksumAddressWithoutPrefix
+      });
+
+    firebase.messaging().sendMessage(upstreamMessage);
   }
 
   registerEthereumAddress(checksumAddress) {
-    const checksumAddressWithoutPrefix = EtherUtilities.stripHexPrefix(checksumAddress);
-
-    const upstreamMessage = new firebase.messaging.RemoteMessage()
-      .setMessageId(this.messageId)
-      .setTo(this.serverAddress)
-      .setData({
-        type: 'address_register',
-        address: checksumAddressWithoutPrefix
-      });
-    firebase.messaging().sendMessage(upstreamMessage);
+    this.__sendMessage('address_register', checksumAddress);
   }
 
   resyncTransactions(checksumAddress) {
-    const checksumAddressWithoutPrefix = EtherUtilities.stripHexPrefix(checksumAddress);
-
-    const upstreamMessage = new firebase.messaging.RemoteMessage()
-      .setMessageId(this.messageId)
-      .setTo(this.serverAddress)
-      .setData({
-        type: 'resync_transactions',
-        address: checksumAddressWithoutPrefix
-      });
-    firebase.messaging().sendMessage(upstreamMessage);
+    this.__sendMessage('resync_transactions', checksumAddress);
   }
 
   requestCDaiLendingInfo(checksumAddress) {
-    const checksumAddressWithoutPrefix = EtherUtilities.stripHexPrefix(checksumAddress);
-
-    const upstreamMessage = new firebase.messaging.RemoteMessage()
-      .setMessageId(this.messageId)
-      .setTo(this.serverAddress)
-      .setData({
-        type: 'cDai_lending_info',
-        address: checksumAddressWithoutPrefix
-      });
-    firebase.messaging().sendMessage(upstreamMessage);
+    this.__sendMessage('cDai_lending_info', checksumAddress);
   }
 }
 
