3651b716a6565eaf58ad942c30a42a5216c460ef
Tai <4382091+taisukemino@users.noreply.github.com>
2020-03-01 21:06:32 +0900

    clean the keychain data when it still exists after the app deletion in ios

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -14,6 +14,7 @@ import {
   HeaderThree,
   TouchableCardContainer
 } from '../components/common';
+import WalletUtilities from '../utilities/WalletUtilities';
 
 export default class Welcome extends Component {
   constructor(props) {
@@ -21,6 +22,13 @@ export default class Welcome extends Component {
     this.state = {
       timePassed: false
     };
+  }
+
+  async componentDidMount() {
+    const privateKeySaved = await WalletUtilities.privateKeySaved();
+    if (privateKeySaved) {
+      await WalletUtilities.resetKeychainData();
+    }
   }
 
   hollaFadeInOut() {
