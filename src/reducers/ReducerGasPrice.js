8b7e00050271641a1494eca4b6b77b09fa0d6898
Tai <4382091+taisukemino@users.noreply.github.com>
2020-03-02 00:45:42 +0900

    add a new reducer to update the gas price chosen

--- src/reducers/ReducerGasPrice.js
+++ src/reducers/ReducerGasPrice.js
@@ -2,14 +2,16 @@
 import {
   GET_GAS_PRICE_FAST,
   GET_GAS_PRICE_AVERAGE,
-  GET_GAS_PRICE_SLOW
+  GET_GAS_PRICE_SLOW,
+  UPDATE_GAS_PRICE_CHOSEN
 } from '../constants/ActionTypes';
 
 const INITIAL_STATE = {
   gasPrice: {
     fast: '0',
     average: '0',
-    slow: '0'
+    slow: '0',
+    chosen: 1
   }
 };
 
@@ -21,6 +23,8 @@ const gasPrice = (state = INITIAL_STATE, action) => {
       return { gasPrice: { ...state.gasPrice, average: action.payload } };
     case GET_GAS_PRICE_SLOW:
       return { gasPrice: { ...state.gasPrice, slow: action.payload } };
+    case UPDATE_GAS_PRICE_CHOSEN:
+      return { gasPrice: { ...state.gasPrice, chosen: action.payload } };
     default:
       return state || INITIAL_STATE;
   }
