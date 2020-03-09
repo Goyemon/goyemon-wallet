87f25155651b13ff2d07065644d4ac52569bb853
Tai <>
2020-03-09 21:53:14 +0900

    refactor the Initial component

--- src/components/Initial.js
+++ src/components/Initial.js
@@ -2,125 +2,124 @@
 import BigNumber from 'bignumber.js';
 import React, { Component } from 'react';
 import * as Animatable from 'react-native-animatable';
-import firebase from 'react-native-firebase';
 import { StackActions, NavigationActions } from 'react-navigation';
+import { connect } from 'react-redux';
 import styled from 'styled-components/native';
+import { Container, Loader } from '../components/common';
 import HomeStack from '../navigators/HomeStack';
-import { store } from '../store/store.js';
+import LogUtilities from '../utilities/LogUtilities.js';
 import WalletUtilities from '../utilities/WalletUtilities.ts';
-
 import TxStorage from '../lib/tx.js';
 
-export default class Initial extends Component {
-  stateTree = store.getState();
-  balance = this.stateTree.ReducerBalance.balance;
-  checksumAddress = this.stateTree.ReducerChecksumAddress.checksumAddress;
-  mnemonicWords = this.stateTree.ReducerMnemonic.mnemonicWords;
-  mnemonicWordsValidation = this.stateTree.ReducerMnemonicWordsValidation.mnemonicWordsValidation;
-  notificationEnabled = this.stateTree.ReducerPermissions.permissions.notification;
-  price = this.stateTree.ReducerPrice.price;
-  transactionsLoaded = this.stateTree.ReducerTransactionsLoaded.transactionsLoaded;
+class Initial extends Component {
+  async componentDidUpdate(prevProps) {
+    if (this.props.rehydration != prevProps.rehydration) {
+      await this.conditionalNavigation();
+    }
+  }
 
   async componentDidMount() {
-    TxStorage.storage.setOwnAddress(this.checksumAddress); 
+    TxStorage.storage.setOwnAddress(this.props.checksumAddress);
+    await this.conditionalNavigation();
+  }
 
-    let mnemonicWordsStatePersisted;
-    if (this.mnemonicWords === null) {
-      mnemonicWordsStatePersisted = false;
-    } else if (this.mnemonicWords != null) {
-      mnemonicWordsStatePersisted = true;
-    }
-
-    const hasPersistedState = this.hasPersistedState();
-
-    const hasPrivateKeyInKeychain = await WalletUtilities.privateKeySaved();
-
-    const enabled = await firebase.messaging().hasPermission();
-    if (enabled === true) {
-      this.notificationEnabled = true;
-    }
-
-    let mainPage = 'Welcome';
-
-    if (
-      (!mnemonicWordsStatePersisted && !this.notificationEnabled && !hasPrivateKeyInKeychain) ||
-      (mnemonicWordsStatePersisted &&
-        !this.mnemonicWordsValidation &&
-        !this.notificationEnabled &&
-        !hasPrivateKeyInKeychain) ||
-      (mnemonicWordsStatePersisted &&
-        this.notificationEnabled === null &&
-        hasPrivateKeyInKeychain) ||
-      (!mnemonicWordsStatePersisted && this.notificationEnabled && !hasPrivateKeyInKeychain) ||
-      (!mnemonicWordsStatePersisted && !this.notificationEnabled && hasPrivateKeyInKeychain)
-    ) {
-      mainPage = 'Welcome';
-    } else if (
-      mnemonicWordsStatePersisted &&
-      !this.notificationEnabled &&
-      hasPrivateKeyInKeychain
-    ) {
-      mainPage = 'NotificationPermissionNotGranted';
-    } else if (
-      (mnemonicWordsStatePersisted &&
-        this.mnemonicWordsValidation &&
-        this.notificationEnabled &&
-        !hasPersistedState &&
-        !hasPrivateKeyInKeychain) ||
-      (mnemonicWordsStatePersisted &&
-        this.notificationEnabled &&
-        !hasPersistedState &&
-        hasPrivateKeyInKeychain)
-    ) {
-      mainPage = 'WalletCreation';
-    } else if (
-      mnemonicWordsStatePersisted &&
-      this.notificationEnabled &&
-      hasPersistedState &&
-      hasPrivateKeyInKeychain
-    ) {
-      mainPage = 'WalletList';
-    }
-
-    HomeStack.navigationOptions = ({ navigation }) => {
-      let tabBarVisible;
-      if (navigation.state.index >= 0 && mainPage === 'WalletList') {
-        tabBarVisible = true;
-      } else if (
-        (navigation.state.index >= 0 && mainPage === 'Welcome') ||
-        'NotificationPermissionNotGranted' ||
-        'WalletCreation'
-      ) {
-        tabBarVisible = false;
+  async conditionalNavigation() {
+    if (this.props.rehydration) {
+      let mnemonicWordsStatePersisted;
+      if (this.props.mnemonicWords === null) {
+        mnemonicWordsStatePersisted = false;
+      } else if (this.props.mnemonicWords != null) {
+        mnemonicWordsStatePersisted = true;
       }
 
-      return {
-        tabBarVisible
-      };
-    };
+      const hasPersistedState = this.hasPersistedState();
 
-    const resetAction = StackActions.reset({
-      index: 0,
-      actions: [NavigationActions.navigate({ routeName: mainPage })]
-    });
-    this.props.navigation.dispatch(resetAction);
+      const hasPrivateKeyInKeychain = await WalletUtilities.privateKeySaved();
+
+      let mainPage = 'Welcome';
+
+      if (
+        !mnemonicWordsStatePersisted ||
+        (mnemonicWordsStatePersisted &&
+          !this.props.mnemonicWordsValidation &&
+          !this.props.permissions.notification &&
+          !hasPrivateKeyInKeychain) ||
+        (mnemonicWordsStatePersisted &&
+          this.props.permissions.notification === null &&
+          hasPrivateKeyInKeychain)
+      ) {
+        mainPage = 'Welcome';
+      } else if (
+        mnemonicWordsStatePersisted &&
+        !this.props.permissions.notification &&
+        hasPrivateKeyInKeychain
+      ) {
+        mainPage = 'NotificationPermissionNotGranted';
+      } else if (
+        (mnemonicWordsStatePersisted &&
+          this.props.mnemonicWordsValidation &&
+          this.props.permissions.notification &&
+          !hasPersistedState &&
+          !hasPrivateKeyInKeychain) ||
+        (mnemonicWordsStatePersisted &&
+          this.props.permissions.notification &&
+          !hasPersistedState &&
+          hasPrivateKeyInKeychain)
+      ) {
+        mainPage = 'WalletCreation';
+      } else if (
+        mnemonicWordsStatePersisted &&
+        this.props.permissions.notification &&
+        hasPersistedState &&
+        hasPrivateKeyInKeychain
+      ) {
+        mainPage = 'WalletList';
+      }
+
+      HomeStack.navigationOptions = ({ navigation }) => {
+        let tabBarVisible;
+        if (navigation.state.index >= 0 && mainPage === 'WalletList') {
+          tabBarVisible = true;
+        } else if (
+          (navigation.state.index >= 0 && mainPage === 'Welcome') ||
+          'NotificationPermissionNotGranted' ||
+          'WalletCreation'
+        ) {
+          tabBarVisible = false;
+        }
+
+        return {
+          tabBarVisible
+        };
+      };
+
+      const resetAction = StackActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({ routeName: mainPage })]
+      });
+      this.props.navigation.dispatch(resetAction);
+    } else if (!this.props.rehydration) {
+      LogUtilities.logInfo('rehydration is not done yet')
+    }
   }
 
   hasPersistedState() {
     return (
       this.hasTransactions() &&
-      this.hasBalance() && this.hasChecksumAddress() && this.hasPrice()
+      this.hasBalance() &&
+      this.hasChecksumAddress() &&
+      this.hasPrice()
     );
   }
 
   hasTransactions() {
-    return this.transactionsLoaded != null;
+    return this.props.transactionsLoaded != null;
   }
 
   hasBalance = () => {
-    const cDaiBalance = new BigNumber(this.balance.cDaiBalance);
-    const daiBalance = new BigNumber(this.balance.daiBalance);
-    const weiBalance = new BigNumber(this.balance.weiBalance);
+    const cDaiBalance = new BigNumber(this.props.balance.cDaiBalance);
+    const daiBalance = new BigNumber(this.props.balance.daiBalance);
+    const weiBalance = new BigNumber(this.props.balance.weiBalance);
     return (
       cDaiBalance.isGreaterThanOrEqualTo(0) &&
       daiBalance.isGreaterThanOrEqualTo(0) &&
@@ -128,27 +127,45 @@ export default class Initial extends Component {
     );
   };
 
-  hasChecksumAddress = () => this.checksumAddress != null;
+  hasChecksumAddress = () => this.props.checksumAddress != null;
 
-  hasPrice = () => (
-      this.price.eth >= 0 &&
-      this.price.eth.length != 0 &&
-      this.price.dai >= 0 &&
-      this.price.dai.length != 0
-    );
+  hasPrice = () =>
+    this.props.price.eth >= 0 &&
+    this.props.price.eth.length != 0 &&
+    this.props.price.dai >= 0 &&
+    this.props.price.dai.length != 0;
 
   render() {
     return (
-        <Title animation="fadeIn" delay={2000}>
-          loading
-        </Title>
+      <Container
+        alignItems="center"
+        flexDirection="column"
+        justifyContent="center"
+        marginTop={0}
+        width="90%"
+      >
+        <LoaderContainer animation="fadeIn" delay={1000}>
+          <Loader animating={true} size="large" />
+        </LoaderContainer>
+      </Container>
     );
   }
 }
 
-const Title = Animatable.createAnimatableComponent(styled.Text`
-  font-family: 'HKGrotesk-Regular';
-  font-size: 40;
-  margin-bottom: 16;
-  text-align: center;
-`);
+const LoaderContainer = Animatable.createAnimatableComponent(styled.View``);
+
+function mapStateToProps(state) {
+  return {
+    balance: state.ReducerBalance.balance,
+    checksumAddress: state.ReducerChecksumAddress.checksumAddress,
+    mnemonicWords: state.ReducerMnemonic.mnemonicWords,
+    mnemonicWordsValidation:
+      state.ReducerMnemonicWordsValidation.mnemonicWordsValidation,
+    permissions: state.ReducerPermissions.permissions,
+    price: state.ReducerPrice.price,
+    rehydration: state.ReducerRehydration.rehydration,
+    transactionsLoaded: state.ReducerTransactionsLoaded.transactionsLoaded
+  };
+}
+
+export default connect(mapStateToProps)(Initial);
