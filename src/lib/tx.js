1b2d7ee8d25e822e4c1eebf7d54230bfabe3ad7a
toruvinn <github@lain.pl>
2020-03-11 22:32:44 +0900

    store time tx is created (in newTx()) not to change the sort order because SOME PEOPLE (hey, Tai) are complaining

--- src/lib/tx.js
+++ src/lib/tx.js
@@ -369,6 +369,11 @@ class Tx {
 		return this;
 	}
 
+	setSentTimestamp(tstamp) {
+		this.sent_timestamp = tstamp;
+		return this;
+	}
+
 	setState(state) {
 		this.state = state;
 		return this;
@@ -401,7 +406,7 @@ class Tx {
 						)
 					)
 				);
-			else
+			else {
 				Object.entries(data[8]).forEach(
 					([token, ops]) => ops.forEach(
 						opdescr => Object.entries(opdescr).forEach(
@@ -409,6 +414,9 @@ class Tx {
 						)
 					)
 				);
+				if (data.length > 9)
+					this.setSentTimestamp(data[9]);
+			}
 		}
 
 		return this.setFrom(data[0])
@@ -472,6 +480,10 @@ class Tx {
 
 	getTimestamp() {
 		return this.timestamp;
+	}
+
+	getSortTimestamp() {
+		return this.sent_timestamp ? this.sent_timestamp : this.timestamp;
 	}
 
 	getNonce() {
@@ -625,9 +637,12 @@ class TxStorage {
 	}
 
 	async newTx(state=TxStates.STATE_NEW, nonce) {
+		const now = Math.trunc(Date.now() / 1000);
 		let tx = new Tx(state)
-			.setTimestamp(Math.trunc(Date.now() / 1000))
+			.setTimestamp(now)
+			.setSentTimestamp(now)
 			.setNonce(nonce ? nonce : await this.getNextNonce());
+
 		tx.from_addr = this.our_address;
 
 		return tx;
@@ -917,7 +932,7 @@ class TxStorage {
 		LogUtilities.toDebugScreen(`tempGetAllAsList() not included txes: `, nit);
 		nit.forEach((x) => ret.push(x));
 
-		ret.sort((a, b) => b.getTimestamp() - a.getTimestamp());
+		ret.sort((a, b) => b.getSortTimestamp() - a.getSortTimestamp());
 
 		return ret;
 	}
