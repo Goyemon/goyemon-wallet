7055032d821add3a26b4c143471b5a076c005137
toruvinn <github@lain.pl>
2020-03-11 12:27:58 +0900

    remade background listener too; hopefully im not wrong here, need to check later.

--- src/firebase/FcmBackgroundListener.js
+++ src/firebase/FcmBackgroundListener.js
@@ -1,8 +1,12 @@
 'use strict';
 import FcmListener from './FcmListener.js';
+import FCM from '../lib/fcm.js';
+
+FCM.FCMMsgs.setMsgCallback(FcmListener.downstreamMessageHandler); // so now FcmListener is just a callback we attach to FCMMsgs.
+FCM.registerHandler(); // Then we call FCM.registerHandler() to actually initialize FCM.
 
 export default async downstreamMessage => {
-  await FcmListener.downstreamMessageHandler(downstreamMessage);
+  await FCM.downstreamMessageHandler(downstreamMessage);
 
   return Promise.resolve();
 }
