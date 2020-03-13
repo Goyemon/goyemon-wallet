baac44adbd26120e237eec5de06010775b68c107
Tai <>
2020-03-13 17:32:40 +0900

    remove the MnemonicWordsScreenShot component

--- src/navigators/HomeStack.js
+++ src/navigators/HomeStack.js
@@ -9,7 +9,6 @@ import ImportOptions from '../components/ImportOptions';
 import ImportTwentyFourMnemonicWords from '../containers/ImportTwentyFourMnemonicWords';
 import ImportTwelveMnemonicWords from '../containers/ImportTwelveMnemonicWords';
 import CreateWalletTutorial from '../containers/CreateWalletTutorial';
-import MnemonicWordsScreenshot from '../containers/MnemonicWordsScreenshot';
 import ShowMnemonic from '../components/ShowMnemonic';
 import VerifyMnemonic from '../containers/VerifyMnemonic';
 import WalletList from '../containers/WalletList';
@@ -77,12 +76,6 @@ const HomeStack = createStackNavigator(
     },
     CreateWalletTutorial: {
       screen: CreateWalletTutorial,
-      navigationOptions: () => ({
-        headerTransparent: true
-      })
-    },
-    MnemonicWordsScreenshot: {
-      screen: MnemonicWordsScreenshot,
       navigationOptions: () => ({
         headerTransparent: true
       })
