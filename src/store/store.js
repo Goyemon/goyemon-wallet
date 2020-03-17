ff8c1edc1595f5a3a62ac2c87fe7128242ba2d49
Tai <>
2020-03-17 18:32:32 +0900

    specify the storage as AsyncStorage in the redux-persist config

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
 'use strict';
+import AsyncStorage from '@react-native-community/async-storage';
 import { applyMiddleware, createStore } from 'redux';
 import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducers from '../reducers/ReducerIndex';
 
 const persistConfig = {
   key: 'root',
-  storage,
+  storage: AsyncStorage,
   whitelist: [
     'ReducerBalance',
     'ReducerCDaiLendingInfo',
