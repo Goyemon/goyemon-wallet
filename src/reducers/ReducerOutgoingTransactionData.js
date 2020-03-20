8a6b9cca132705a11dd795c752e73c376ae1691a
Tai <>
2020-03-20 16:25:18 +0900

    refactor the OutgoingTransactionData reducer for the new actions

--- src/reducers/ReducerOutgoingTransactionData.js
+++ src/reducers/ReducerOutgoingTransactionData.js
@@ -1,37 +1,59 @@
 'use strict';
-import { SAVE_OUTGOING_TRANSACTION_DATA_AMOUNT } from '../constants/ActionTypes';
-import { SAVE_OUTGOING_TRANSACTION_DATA_TOADDRESS } from '../constants/ActionTypes';
-import { SAVE_OUTGOING_TRANSACTION_DATA_APPROVE_AMOUNT } from '../constants/ActionTypes';
+import { SAVE_OUTGOING_TRANSACTION_DATA_SEND } from '../constants/ActionTypes';
+import { SAVE_OUTGOING_TRANSACTION_DATA_COMPOUND } from '../constants/ActionTypes';
+import { SAVE_OUTGOING_TRANSACTION_DATA_SWAP } from '../constants/ActionTypes';
+import { SAVE_OUTGOING_TRANSACTION_DATA_SWAP_SLIPPAGE } from '../constants/ActionTypes';
 
 const INITIAL_STATE = {
   outgoingTransactionData: {
-    amount: '',
-    toaddress: '',
-    approveAmount: ''
+    send: {
+      toaddress: '',
+      amount: ''
+    },
+    compound: {
+      amount: ''
+    },
+    swap: {
+      sold: '',
+      bought: '',
+      slippage: 0.5,
+      minBought: ''
+    }
   }
 };
 
 const outgoingTransactionData = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case SAVE_OUTGOING_TRANSACTION_DATA_AMOUNT:
+    case SAVE_OUTGOING_TRANSACTION_DATA_SEND:
       return {
         outgoingTransactionData: {
           ...state.outgoingTransactionData,
-          amount: action.payload
+          send: action.payload
         }
       };
-    case SAVE_OUTGOING_TRANSACTION_DATA_TOADDRESS:
+    case SAVE_OUTGOING_TRANSACTION_DATA_COMPOUND:
       return {
         outgoingTransactionData: {
           ...state.outgoingTransactionData,
-          toaddress: action.payload
+          compound: action.payload
         }
       };
-    case SAVE_OUTGOING_TRANSACTION_DATA_APPROVE_AMOUNT:
+    case SAVE_OUTGOING_TRANSACTION_DATA_SWAP:
       return {
         outgoingTransactionData: {
-          ...state.outgoingTransactionData,
-          approveAmount: action.payload
+          swap: {
+            ...state.outgoingTransactionData.swap,
+            ...action.payload
+          }
+        }
+      };
+    case SAVE_OUTGOING_TRANSACTION_DATA_SWAP_SLIPPAGE:
+      return {
+        outgoingTransactionData: {
+          swap: {
+            ...state.outgoingTransactionData.swap,
+            slippage: action.payload
+          }
         }
       };
     default:
