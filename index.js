092c4f08d87c02a257905649782dfe16b60ea79b
toruvinn <github@lain.pl>
2020-03-21 12:40:22 +0900

    await rehydration in fcm processing

--- index.js
+++ index.js
@@ -9,18 +9,26 @@ import { rehydrationComplete } from './src/actions/ActionRehydration';
 import App from './src/navigators/AppTab';
 import { name as appName } from './app.json';
 import FcmListener from './src/firebase/FcmListener';
+import FCM from './src/lib/fcm';
 import FcmBackgroundListener from './src/firebase/FcmBackgroundListener';
 import { store } from './src/store/store';
+import TxStorage from './src/lib/tx';
 
 YellowBox.ignoreWarnings(['Remote debugger']);
 
 AppRegistry.registerComponent(appName, () => App);
 AppRegistry.registerHeadlessTask(
-  'RNFirebaseBackgroundMessage',
-  () => FcmBackgroundListener
+	'RNFirebaseBackgroundMessage',
+	() => FcmBackgroundListener
 );
 
-persistStore(store, {}, () => {
-  store.dispatch(rehydrationComplete(true));
-  FcmListener.registerHandler();
-});
+FCM.FCMMsgs.setMsgCallback(FcmListener.downstreamMessageHandler); // so now FcmListener is just a callback we attach to FCMMsgs.
+FcmListener.setStoreReadyPromise(new Promise((resolve, reject) => {
+	persistStore(store, {}, () => {
+		TxStorage.storage.isStorageReady().then(() => {
+			store.dispatch(rehydrationComplete(true));
+			resolve();
+		});
+	});
+}));
+FCM.registerHandler(); // Then we call FCM.registerHandler() to actually initialize FCM.
