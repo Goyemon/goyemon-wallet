9dc1ee92132aa948b099293c928ef049b9f35e6a
Tai <>
2020-03-27 15:13:09 +0900

    move components from the HomeStack to the BalanceStack

--- src/navigators/BalanceStack.js
+++ src/navigators/BalanceStack.js
@@ -1,5 +1,16 @@
 'use strict';
 import { createStackNavigator } from 'react-navigation';
+import Initial from '../components/Initial';
+import Welcome from '../components/Welcome';
+import NotificationPermissionTutorial from '../containers/NotificationPermissionTutorial';
+import NotificationPermissionNotGranted from '../components/NotificationPermissionNotGranted';
+import WalletCreation from '../containers/WalletCreation';
+import ImportOptions from '../components/ImportOptions';
+import ImportTwentyFourMnemonicWords from '../containers/ImportTwentyFourMnemonicWords';
+import ImportTwelveMnemonicWords from '../containers/ImportTwelveMnemonicWords';
+import CreateWalletTutorial from '../containers/CreateWalletTutorial';
+import ShowMnemonic from '../containers/ShowMnemonic';
+import VerifyMnemonic from '../containers/VerifyMnemonic';
 import BalanceHome from '../containers/BalanceHome';
 import Receive from '../containers/Receive';
 import BalanceWallet from '../containers/BalanceWallet';
@@ -7,6 +18,75 @@ import BalanceCompound from '../containers/BalanceCompound';
 
 const BalanceStack = createStackNavigator(
   {
+    Initial: {
+      screen: Initial,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
+    Welcome: {
+      screen: Welcome,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
+    NotificationPermissionTutorial: {
+      screen: NotificationPermissionTutorial,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
+    NotificationPermissionNotGranted: {
+      screen: NotificationPermissionNotGranted,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
+    WalletCreation: {
+      screen: WalletCreation,
+      navigationOptions: () => ({
+        header: null
+      })
+    },
+    ImportOptions: {
+      screen: ImportOptions,
+      navigationOptions: () => ({
+        headerTransparent: true,
+        headerBackTitle: 'ImportOptions'
+      })
+    },
+    ImportTwentyFourMnemonicWords: {
+      screen: ImportTwentyFourMnemonicWords,
+      navigationOptions: () => ({
+        headerTransparent: true,
+        headerBackTitle: 'Import'
+      })
+    },
+    ImportTwelveMnemonicWords: {
+      screen: ImportTwelveMnemonicWords,
+      navigationOptions: () => ({
+        headerTransparent: true,
+        headerBackTitle: 'Import'
+      })
+    },
+    CreateWalletTutorial: {
+      screen: CreateWalletTutorial,
+      navigationOptions: () => ({
+        headerTransparent: true
+      })
+    },
+    ShowMnemonic: {
+      screen: ShowMnemonic,
+      navigationOptions: () => ({
+        headerTransparent: true
+      })
+    },
+    VerifyMnemonic: {
+      screen: VerifyMnemonic,
+      navigationOptions: () => ({
+        headerTransparent: true
+      })
+    },
     BalanceHome: {
       screen: BalanceHome,
       navigationOptions: () => ({
--- src/navigators/HomeStack.js
+++ src/navigators/HomeStack.js
@@ -1,16 +1,5 @@
 'use strict';
 import { createStackNavigator } from 'react-navigation';
-import Initial from '../components/Initial';
-import Welcome from '../components/Welcome';
-import NotificationPermissionTutorial from '../containers/NotificationPermissionTutorial';
-import NotificationPermissionNotGranted from '../components/NotificationPermissionNotGranted';
-import WalletCreation from '../containers/WalletCreation';
-import ImportOptions from '../components/ImportOptions';
-import ImportTwentyFourMnemonicWords from '../containers/ImportTwentyFourMnemonicWords';
-import ImportTwelveMnemonicWords from '../containers/ImportTwelveMnemonicWords';
-import CreateWalletTutorial from '../containers/CreateWalletTutorial';
-import ShowMnemonic from '../containers/ShowMnemonic';
-import VerifyMnemonic from '../containers/VerifyMnemonic';
 import WalletList from '../containers/WalletList';
 import SendEth from '../containers/SendEth';
 import QRCodeScan from '../containers/QRCodeScan';
@@ -21,75 +10,6 @@ import SendDaiConfirmation from '../containers/SendDaiConfirmation';
 
 const HomeStack = createStackNavigator(
   {
-    Initial: {
-      screen: Initial,
-      navigationOptions: () => ({
-        header: null
-      })
-    },
-    Welcome: {
-      screen: Welcome,
-      navigationOptions: () => ({
-        header: null
-      })
-    },
-    NotificationPermissionTutorial: {
-      screen: NotificationPermissionTutorial,
-      navigationOptions: () => ({
-        header: null
-      })
-    },
-    NotificationPermissionNotGranted: {
-      screen: NotificationPermissionNotGranted,
-      navigationOptions: () => ({
-        header: null
-      })
-    },
-    WalletCreation: {
-      screen: WalletCreation,
-      navigationOptions: () => ({
-        header: null
-      })
-    },
-    ImportOptions: {
-      screen: ImportOptions,
-      navigationOptions: () => ({
-        headerTransparent: true,
-        headerBackTitle: 'ImportOptions'
-      })
-    },
-    ImportTwentyFourMnemonicWords: {
-      screen: ImportTwentyFourMnemonicWords,
-      navigationOptions: () => ({
-        headerTransparent: true,
-        headerBackTitle: 'Import'
-      })
-    },
-    ImportTwelveMnemonicWords: {
-      screen: ImportTwelveMnemonicWords,
-      navigationOptions: () => ({
-        headerTransparent: true,
-        headerBackTitle: 'Import'
-      })
-    },
-    CreateWalletTutorial: {
-      screen: CreateWalletTutorial,
-      navigationOptions: () => ({
-        headerTransparent: true
-      })
-    },
-    ShowMnemonic: {
-      screen: ShowMnemonic,
-      navigationOptions: () => ({
-        headerTransparent: true
-      })
-    },
-    VerifyMnemonic: {
-      screen: VerifyMnemonic,
-      navigationOptions: () => ({
-        headerTransparent: true
-      })
-    },
     WalletList: {
       screen: WalletList,
       navigationOptions: () => ({
