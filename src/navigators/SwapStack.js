d0890b56e981ff4580a9c8ca58ffb254c35b2aba
Tai <>
2020-03-28 14:21:06 +0900

    add components from the SettingsStack to other stacks

--- src/navigators/SwapStack.js
+++ src/navigators/SwapStack.js
@@ -2,6 +2,9 @@
 import { createStackNavigator } from 'react-navigation';
 import Swap from '../containers/Swap';
 import SwapConfirmation from '../containers/SwapConfirmation';
+import Settings from '../containers/Settings';
+import BackupWords from '../containers/BackupWords';
+import Advanced from '../containers/Advanced';
 
 const SwapStack = createStackNavigator(
   {
@@ -17,6 +20,24 @@ const SwapStack = createStackNavigator(
       navigationOptions: () => ({
         headerTransparent: true,
         headerBackTitle: 'SwapConfirmation'
+      })
+    },
+    Settings: {
+      screen: Settings,
+      navigationOptions: () => ({
+        headerTransparent: true
+      })
+    },
+    BackupWords: {
+      screen: BackupWords,
+      navigationOptions: () => ({
+        headerTransparent: true
+      })
+    },
+    Advanced: {
+      screen: Advanced,
+      navigationOptions: () => ({
+        headerTransparent: true
       })
     }
   },
