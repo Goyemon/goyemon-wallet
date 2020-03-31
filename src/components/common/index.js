7b12282723999084b6bb143ce3041cf95c272ffe
Tai <>
2020-03-31 22:35:04 +0900

    remove the common settings icon component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -18,7 +18,6 @@ export * from './Loader';
 export * from './MenuContainer';
 export * from './ProgressBar';
 export * from './QRCodeIcon';
-export * from './SettingsIcon';
 export * from './SwapForm';
 export * from './RootContainer';
 export * from './ToggleCurrencySymbol';
