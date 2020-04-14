cbb228926f176eb6fcfb4e644b0af17acca92447
Tai <4382091+taisukemino@users.noreply.github.com>
2020-04-14 17:19:47 +0900

    a new reducer for pooltogether

---
+++ src/reducers/ReducerPoolTogether.js
@@ -0,0 +1,34 @@
+'use strict';
+import { SAVE_POOLTOGETHER_DAI_INFO } from '../constants/ActionTypes';
+
+const INITIAL_STATE = {
+  poolTogether: {
+    dai: {
+      estimatedPrize: '',
+      ticketsSold: '',
+      players: '',
+      countdown: ''
+    }
+  },
+};
+
+const poolTogether = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case SAVE_POOLTOGETHER_DAI_INFO:
+      return {
+        poolTogether: {
+          dai: {
+            ...state.poolTogether.dai,
+            estimatedPrize: action.payload.estimatedPrize,
+            ticketsSold: action.payload.tickets_sold,
+            players: action.payload.players,
+            countdown: action.payload.countdown
+          }
+        },
+      };
+    default:
+      return state || INITIAL_STATE;
+  }
+};
+
+export default poolTogether;
