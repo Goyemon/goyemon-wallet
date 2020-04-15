5a40767024649f1ed971e316f7a13aec263221e7
Tai <4382091+taisukemino@users.noreply.github.com>
2020-04-15 18:45:16 +0900

    change the button style conditionally with the locale

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,6 +1,7 @@
 'use strict';
 import React from 'react';
 import styled from 'styled-components';
+import I18n from '../../i18n/I18n';
 
 const Button = (props) => (
   <ButtonContainer
@@ -25,14 +26,25 @@ const ButtonContainer = styled.TouchableOpacity`
   margin-bottom: ${(props) => props.marginBottom};
   min-width: 120px;
   opacity: ${(props) => props.opacity};
-`;
+  `;
 
-const ButtonText = styled.Text`
-  color: ${(props) => props.textColor};
-  font-family: 'HKGrotesk-Bold';
-  font-size: 20;
-  padding: 12px 24px;
-  text-align: center;
-`;
+let ButtonText;
+if (I18n.locale === 'ja-US') {
+  ButtonText = styled.Text`
+    color: ${(props) => props.textColor};
+    font-family: 'HKGrotesk-Bold';
+    font-size: 18;
+    padding: 16px 24px 12px 24px;
+    text-align: center;
+  `;
+} else {
+  ButtonText = styled.Text`
+    color: ${(props) => props.textColor};
+    font-family: 'HKGrotesk-Bold';
+    font-size: 20;
+    padding: 12px 24px;
+    text-align: center;
+  `;
+}
 
 export { Button };
