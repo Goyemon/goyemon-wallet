525efa6515beea292fbec1f5df36d1cae2634575
Tai <4382091+taisukemino@users.noreply.github.com>
2020-06-07 14:53:22 +0900

    add the new BigNumber clone, RoundDownBigNumberPlacesEighteen

--- src/utilities/BigNumberUtilities.js
+++ src/utilities/BigNumberUtilities.js
@@ -5,3 +5,8 @@ export const RoundDownBigNumberPlacesFour = BigNumber.clone({
   DECIMAL_PLACES: 4,
   ROUNDING_MODE: BigNumber.ROUND_DOWN
 });
+
+export const RoundDownBigNumberPlacesEighteen = BigNumber.clone({
+  DECIMAL_PLACES: 18,
+  ROUNDING_MODE: BigNumber.ROUND_DOWN
+});
