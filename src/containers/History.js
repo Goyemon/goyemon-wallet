c4bed7173ab7b91c4ad89267c5684b358a497143
Tai <4382091+taisukemino@users.noreply.github.com>
2020-06-15 14:18:24 +0900

    fix the toBN error in android. round down gasPrice before it is passed to the toBN

--- src/containers/History.js
+++ src/containers/History.js
@@ -403,6 +403,7 @@ const MagicalGasPriceSlider = connect(mapGasPriceStateToProps)(
     }
 
     sliderValueChange(gasPriceWeiDecimal) {
+      gasPriceWeiDecimal = Math.floor(gasPriceWeiDecimal);
       this.setState(this.getPriceState(gasPriceWeiDecimal));
     }
 
