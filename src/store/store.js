c847b2dfa14ae853f255e900cc9f0a97ec774442
Tai <4382091+taisukemino@users.noreply.github.com>
2020-07-13 15:01:35 +0900

    add the redux persist migration for the cdai price

--- src/store/store.js
+++ src/store/store.js
@@ -5,13 +5,19 @@ import { createMigrate, persistReducer, persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 import rootReducers from '../reducers/ReducerIndex';
 
-// const migrations = {
-//   0: (state) => {
-//     return {
-//       ...state,
-//     }
-//   }
-// }
+const migrations = {
+  0: (state) => {
+    return {
+      ...state,
+      ReducerPrice: {
+        price: {
+          ...state.price,
+          cdai: ''
+        }
+      }
+    };
+  }
+};
 
 const persistConfig = {
   key: 'root',
@@ -29,8 +35,8 @@ const persistConfig = {
     'ReducerTransactionsLoaded',
     'ReducerUniswap'
   ],
-  // version: 0 // default version is -1
-  // migrate: createMigrate(migrations, { debug: true })
+  version: 0, // default version is -1
+  migrate: createMigrate(migrations, { debug: true })
 };
 
 // 'ReducerTransactionHistory',
