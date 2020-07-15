3ebbcf31445477464c5b2d450707708e9ad22d25
NoCtrlZ <phantomofrotten@gmail.com>
2020-07-15 14:37:19 +0900

    separate transaction detail header

--- src/containers/TransactionDetailContainer.js
+++ src/containers/TransactionDetailContainer.js
@@ -12,6 +12,7 @@ import {
 import LogUtilities from '../utilities/LogUtilities';
 import GlobalConfig from '../config.json';
 import SwapBox from '../components/TransactionDetailContainer/SwapBox'
+import TransactionDetailHeader from '../components/TransactionDetailContainer/TransactionDetailHeader'
 
 export default class TransactionDetailContainer extends Component {
     componentDidMount() {
@@ -130,12 +131,6 @@ margin-top: 4;
 margin-bottom: 16;
 `;
 
-const HeaderStatus = styled.View`
-align-items: center;
-font-family: 'HKGrotesk-Regular';
-width: 40%;
-`;
-
 const LabelsBox = styled.View`
 flex-direction: column;
 margin-left: 22%;
@@ -163,65 +158,4 @@ margin-top: 32;
 margin-bottom: 32;
 align-items: center;
 width: 90%;
-`;
-
-class TransactionDetailHeader extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      icon: props.icon,
-      timestamp: props.timestamp,
-      status: props.status,
-      method: props.method
-    }
-  }
-
-  render() {
-    const { timestamp, status, method } = this.props
-    const { name, size, color } = this.props.icon
-    return (
-      <TxDetailHeader>
-        <TxIcon>
-          <Icon name={name} size={size + 8} color={color}/>
-        </TxIcon>
-        <TypeAndTime>
-          <GoyemonText fontSize={18}>
-            {method}
-          </GoyemonText>
-          <GoyemonText fontSize={15}>
-            {timestamp}
-          </GoyemonText>
-        </TypeAndTime>
-        <HeaderStatus>
-          <TransactionStatus
-            width="100%"
-            txState={status}
-          />
-        </HeaderStatus>
-      </TxDetailHeader>
-    )
-  }
-}
-
-const TxDetailHeader = styled.View`
-align-items: center;
-background-color: #f8f8f8;
-flex-direction: row;
-justify-content: center;
-font-family: 'HKGrotesk-Regular';
-padding-top: 24px;
-padding-bottom: 24px;
-width: 100%;
-`;
-
-const TxIcon = styled.View`
-align-items: center;
-width: 20%;
-`;
-
-const TypeAndTime = styled.View`
-align-items: flex-start;
-font-family: 'HKGrotesk-Regular';
-flex-direction: column;
-width: 40%;
 `;
