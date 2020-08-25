0f3d89753766918f9525a8fc7fd65a3e87450a25
Tai <4382091+taisukemino@users.noreply.github.com>
2020-08-25 16:00:06 +0900

    fix the #289

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['@babel/plugin-transform-flow-strip-types'],
   env: {
     production: {
       plugins: ['@babel/plugin-proposal-class-properties']
